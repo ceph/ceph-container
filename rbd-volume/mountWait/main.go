26a3757f19184e9dfd8994040b69fa2e22a9fc17
Dmitry K <d.p.karpov@gmail.com>
2015-03-27 12:36:59 +0300

    rbd-volume: the possibility to mount the rbd image in read-only mode

--- rbd-volume/mountWait/main.go
+++ rbd-volume/mountWait/main.go
@@ -14,19 +14,27 @@ var (
 	RBDDev   string
 	FSType string
 	Target string
+	Readonly bool
 )
 
 func init() {
 	flag.StringVar(&RBDDev, "rbddev", "rbd", "RBD dev to mount")
 	flag.StringVar(&FSType, "fstype", "xfs", "Filesystem type")
 	flag.StringVar(&Target, "target", "/mnt/rbd", "Mountpoint / target")
+	flag.BoolVar(&Readonly, "r", false, "Mount the file system read-only")
 }
 
 func main() {
 	flag.Parse()
 
+	var syscall_flags uintptr = 0
+
+	if Readonly {
+		syscall_flags |= syscall.MS_RDONLY
+	}
+
 	// Mount the RBD
-	err := syscall.Mount(RBDDev, Target, FSType, 0, "")
+	err := syscall.Mount(RBDDev, Target, FSType, syscall_flags, "")
 	if err != nil {
 		log.Fatalf("Failed to mount: %s\n", err.Error())
 	}
