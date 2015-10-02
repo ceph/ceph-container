608f4a38b085e263075b25372a0bbee44125fef5
Hunter Nield <hunternield@gmail.com>
2015-10-02 21:48:15 +0800

    k8s config tweaks

--- daemon/config.k8s.sh
+++ daemon/config.k8s.sh
@@ -8,15 +8,15 @@ function get_admin_key {
 
 function get_mon_config {
   
-  MONMAP_ADD=$(kubectl get pods --namespace=${CLUSTER} -l daemon=mon -o template --template="{{range .items}}--add {{.metadata.name}} {{.status.podIP}}{{end}}")
+  MONMAP_ADD=$(kubectl get pods --namespace=${CLUSTER} -l daemon=mon -o template --template="{{range .items}}--add {{.metadata.name}} {{.status.podIP}} {{end}}")
   # MONMAP_ADD="${HOSTS%?}"
 
   monmaptool --create ${MONMAP_ADD} --fsid ${FSID} /etc/ceph/monmap
 
   # Update hostname in ceph.conf
-  sed -i "s/{{ HOSTNAME }}/${MON_HOST}/g" /etc/ceph/ceph.conf
-  sed -i "s/{{ CEPH_PUBLIC_NETWORK }}/${CEPH_PUBLIC_NETWORK}/g" /etc/ceph/ceph.conf
-  sed -i "s/{{ CEPH_CLUSTER_NETWORK }}/${CEPH_CLUSTER_NETWORK}/g" /etc/ceph/ceph.conf
+  sed -i "s/HOSTNAME/${MON_HOST}/g" /etc/ceph/ceph.conf
+  # sed -i "s/\[\[CEPH_PUBLIC_NETWORK\]\]/${CEPH_PUBLIC_NETWORK}/g" /etc/ceph/ceph.conf
+  # sed -i "s/[[CEPH_CLUSTER_NETWORK]]/${CEPH_CLUSTER_NETWORK}/g" /etc/ceph/ceph.conf
 
 }
 
