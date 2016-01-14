c4f188ba448e9308294cc17b243f99738ab88c84
Sébastien Han <seb@redhat.com>
2016-01-14 15:38:36 +0100

    Change ip and network detection logic
    Implementing the network detection for k8s scenarios. Now we do not use
    the MON_IP_AUTODETECT anymore but use a more general variable called
    NETWORK_AUTO_DETECT. Updating the doc.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- daemon/entrypoint.sh
+++ daemon/entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 : ${CEPH_GET_ADMIN_KEY:=0}
 : ${HOSTNAME:=$(hostname -s)}
 : ${MON_NAME:=${HOSTNAME}}
-: ${MON_IP_AUTO_DETECT:=0}
+: ${NETWORK_AUTO_DETECT:=0}
 : ${MDS_NAME:=mds-${HOSTNAME}}
 : ${OSD_FORCE_ZAP:=0}
 : ${OSD_JOURNAL_SIZE:=100}
@@ -110,29 +110,40 @@ function populate_kv {
 #######
 
 function start_mon {
-  if [ ! -n "$CEPH_PUBLIC_NETWORK" ]; then
+  if [[ ! -n "$CEPH_PUBLIC_NETWORK" && ${NETWORK_AUTO_DETECT} -eq 0 ]]; then
     echo "ERROR- CEPH_PUBLIC_NETWORK must be defined as the name of the network for the OSDs"
     exit 1
   fi
 
-  if [ command -v ip ]; then
-    if [ ${MON_IP_AUTO_DETECT} -eq 1 ]; then
-      MON_IP=$(ip -6 -o a | grep scope.global | awk '/eth/ { sub ("/..", "", $4); print $4 }' | head -n1)
-      if [ -z "$MON_IP" ]; then
-        MON_IP=$(ip -4 -o a | awk '/eth/ { sub ("/..", "", $4); print $4 }')
-      fi
-    elif [ ${MON_IP_AUTO_DETECT} -eq 4 ]; then
-      MON_IP=$(ip -4 -o a | awk '/eth/ { sub ("/..", "", $4); print $4 }')
-    elif [ ${MON_IP_AUTO_DETECT} -eq 6 ]; then
-      MON_IP=$(ip -6 -o a | grep scope.global | awk '/eth/ { sub ("/..", "", $4); print $4 }' | head -n1)
-    fi
-  # best effort, only works with ipv4
-  else
-    MON_IP=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' /proc/net/fib_trie | grep -vE "^127|255$|0$")
+  if [[ ! -n "$MON_IP" && ${NETWORK_AUTO_DETECT} -eq 0 ]]; then
+    echo "ERROR- MON_IP must be defined as the IP address of the monitor"
+    exit 1
   fi
 
-  if [[ ! -n "$MON_IP" && ${MON_IP_AUTO_DETECT} -ne 0 ]]; then
-    echo "ERROR- MON_IP must be defined as the IP address of the monitor"
+  if [ ${NETWORK_AUTO_DETECT} -ne 0 ]; then
+    if [ command -v ip ]; then
+      if [ ${NETWORK_AUTO_DETECT} -eq 1 ]; then
+        MON_IP=$(ip -6 -o a | grep scope.global | awk '/eth/ { sub ("/..", "", $4); print $4 }' | head -n1)
+        if [ -z "$MON_IP" ]; then
+          MON_IP=$(ip -4 -o a | awk '/eth/ { sub ("/..", "", $4); print $4 }')
+          CEPH_PUBLIC_NETWORK=$(ip r | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/[0-9]\{1,2\}' | head -1)
+        fi
+      elif [ ${NETWORK_AUTO_DETECT} -eq 4 ]; then
+        MON_IP=$(ip -4 -o a | awk '/eth/ { sub ("/..", "", $4); print $4 }')
+        CEPH_PUBLIC_NETWORK=$(ip r | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/[0-9]\{1,2\}' | head -1)
+      elif [ ${NETWORK_AUTO_DETECT} -eq 6 ]; then
+        MON_IP=$(ip -6 -o a | grep scope.global | awk '/eth/ { sub ("/..", "", $4); print $4 }' | head -n1)
+        CEPH_PUBLIC_NETWORK=$(ip r | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/[0-9]\{1,2\}' | head -1)
+      fi
+    # best effort, only works with ipv4
+    else
+      MON_IP=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' /proc/net/fib_trie | grep -vE "^127|255$|0$")
+      CEPH_PUBLIC_NETWORK=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/[0-9]\{1,2\}' /proc/net/fib_trie | grep -vE "^127|0$" | head -1)
+    fi
+  fi
+
+  if [[ -z "$MON_IP" || -z "$CEPH_PUBLIC_NETWORK" ]]; then
+    echo "ERROR- it looks like we have not been able to discover the network settings"
     exit 1
   fi
 
