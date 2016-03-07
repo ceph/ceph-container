9c0d79f72d0822c4fa3da698725ee18f290bcc80
Jason Murray <jason@murrayinfotech.com>
2016-03-07 12:01:41 +0100

    Changed startup to use for loop. Added rbd to copied wrappers

--- examples/coreos/tools/startup.sh
+++ examples/coreos/tools/startup.sh
@@ -5,17 +5,11 @@ checksum()
 	md5sum $1 | awk '{print $1}'
 }
 
-if [ ! -e /opt/bin/ceph ] || [ "$(checksum /opt/bin/ceph)" != "$(checksum /ceph)" ]; then
-	echo "Installing ceph to /opt/bin"
-	cp -pf /ceph /opt/bin
-fi
+for UTIL in ceph ceph-disk rados rbd; do
 
-if [ ! -e /opt/bin/ceph-disk ] || [ "$(checksum /opt/bin/ceph-disk)" != "$(checksum /ceph-disk)" ]; then
-	echo "Installing ceph-disk to /opt/bin"
-	cp -pf /ceph-disk /opt/bin
-fi
+    if [ ! -e /opt/bin/$UTIL ] || [ "$(checksum /opt/bin/$UTIL)" != "$(checksum /$UTIL)" ]; then
+    	echo "Installing $UTIL to /opt/bin"
+    	cp -pf /$UTIL /opt/bin
+    fi
 
-if [ ! -e /opt/bin/rados ] || [ "$(checksum /opt/bin/rados)" != "$(checksum /rados)" ]; then
-	echo "Installing rados to /opt/bin"
-	cp -pf /rados /opt/bin
-fi
+done
