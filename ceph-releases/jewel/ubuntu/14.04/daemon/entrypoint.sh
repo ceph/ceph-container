b085b57d4eb1318ba607a5338eefa1c7d449ae57
Cooper Su <su.jming@gmail.com>
2016-06-30 16:05:34 +0000

    Combine the same if statements

--- ceph-releases/jewel/ubuntu/14.04/daemon/entrypoint.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/entrypoint.sh
@@ -437,21 +437,18 @@ function osd_activate {
     exit 1
   fi
 
-  # wait till partition exists
-  if [[ ! -z "${OSD_JOURNAL}" ]]; then
-    timeout 10  bash -c "while [ ! -e ${OSD_DEVICE} ]; do sleep 1; done"
-  else
-    timeout 10  bash -c "while [ ! -e $(dev_part ${OSD_DEVICE} 1) ]; do sleep 1; done"
-  fi
   mkdir -p /var/lib/ceph/osd
   chown ceph. /var/lib/ceph/osd
   # resolve /dev/disk/by-* names
   ACTUAL_OSD_DEVICE=$(readlink -f ${OSD_DEVICE})
+  # wait till partition exists then activate it
   if [[ ! -z "${OSD_JOURNAL}" ]]; then
+    timeout 10  bash -c "while [ ! -e ${OSD_DEVICE} ]; do sleep 1; done"
     chown ceph. ${OSD_JOURNAL}
     ceph-disk -v --setuser ceph --setgroup disk activate ${OSD_DEVICE}
     OSD_ID=$(ceph-disk list |grep "${ACTUAL_OSD_DEVICE} ceph data" | awk -F, '{print $4}'|awk -F. '{print $2}')
   else
+    timeout 10  bash -c "while [ ! -e $(dev_part ${OSD_DEVICE} 1) ]; do sleep 1; done"
     chown ceph. $(dev_part ${OSD_DEVICE} 2)
     ceph-disk -v --setuser ceph --setgroup disk activate $(dev_part ${OSD_DEVICE} 1)
     OSD_ID=$(ceph-disk list |grep "$(dev_part ${ACTUAL_OSD_DEVICE} 1) ceph data" | awk -F, '{print $4}'|awk -F. '{print $2}')
