0229bf8da64212a99b4504020125093862419b82
Michael Andersen <michael@steelcode.com>
2016-09-06 21:01:21 -0700

    entrypoint: ignore pgrep exit code
    pgrep can exit 1 if no results are found

--- ceph-releases/jewel/ubuntu/14.04/daemon/entrypoint.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/entrypoint.sh
@@ -553,7 +553,7 @@ function osd_activate {
 
   # ceph-disk activiate has exec'ed /usr/bin/ceph-osd ${CEPH_OPTS} -f -d -i ${OSD_ID}
   # wait till docker stop or ceph-osd is killed
-  OSD_PID=$(pgrep -U ceph -f "^/usr/bin/ceph-osd \-\-cluster ${CLUSTER}.*\-i ${OSD_ID} \-\-setuser")
+  OSD_PID=$(pgrep -U ceph -f "^/usr/bin/ceph-osd \-\-cluster ${CLUSTER}.*\-i ${OSD_ID} \-\-setuser") || true
   if [ -n "${OSD_PID}" ]; then
       echo "OSD (PID ${OSD_PID}) is running, waiting till it exits"
       while [ -e /proc/${OSD_PID} ]; do sleep 1;done
