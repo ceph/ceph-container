4c707e2262afbe7516b6000288555871ed389173
Alfredo Deza <adeza@redhat.com>
2016-11-02 15:38:22 -0400

    tests: add a mon_initial_members test
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- tests/base/test_ceph_conf.py
+++ tests/base/test_ceph_conf.py
@@ -5,4 +5,8 @@ class TestAllContainers(object):
     def test_ceph_fsid_exists(self, mon_containers, client):
         result = client.run(mon_containers, 'grep fsid /etc/ceph/ceph.conf').split()
         assert len(result) == 3
-        assert result[-1] != '='
+
+    def test_initial_members_is_defined(self, mon_containers, client):
+        result = client.run(mon_containers, 'grep "mon initial members" /etc/ceph/ceph.conf').split()
+        host_id = mon_containers['Id'][:12]
+        assert result[-1] == host_id
