8343bb4dbd65cec6bc5319fd3a32fa523f3a3a07
Alfredo Deza <adeza@redhat.com>
2016-11-02 15:40:49 -0400

    tests: add cephx ceph.conf tests
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- tests/base/test_ceph_conf.py
+++ tests/base/test_ceph_conf.py
@@ -10,3 +10,15 @@ class TestAllContainers(object):
         result = client.run(mon_containers, 'grep "mon initial members" /etc/ceph/ceph.conf').split()
         host_id = mon_containers['Id'][:12]
         assert result[-1] == host_id
+
+    def test_cluster_is_required(self, mon_containers, client):
+        result = client.run(mon_containers, 'grep "auth cluster required" /etc/ceph/ceph.conf')
+        assert result == 'auth cluster required = cephx\n'
+
+    def test_service_is_required(self, mon_containers, client):
+        result = client.run(mon_containers, 'grep "auth service required" /etc/ceph/ceph.conf')
+        assert result == 'auth service required = cephx\n'
+
+    def test_client_is_required(self, mon_containers, client):
+        result = client.run(mon_containers, 'grep "auth client required" /etc/ceph/ceph.conf')
+        assert result == 'auth client required = cephx\n'
