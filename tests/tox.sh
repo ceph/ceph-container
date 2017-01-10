1fb5f5a6b50a0a9d5b4d0cb020344739c6c86707
Alfredo Deza <adeza@redhat.com>
2017-01-10 17:08:47 -0500

    tests: include the build steps to get images built
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- tests/tox.sh
+++ tests/tox.sh
@@ -9,21 +9,42 @@
 
 # setup
 #################################################################################
+# XXX this should probably not install system dependencies like this, since now
+# it means we are tied to an apt-get distro
+sudo apt-get install -y --force-yes docker.io
+sudo apt-get install -y --force-yes xfsprogs
 git clone -b $CEPH_ANSIBLE_BRANCH --single-branch https://github.com/ceph/ceph-ansible.git ceph-ansible
 pip install -r $TOXINIDIR/ceph-ansible/tests/requirements.txt
+
+# pull requests tests should never have these directories here, but branches
+# do, so for the build scripts to work correctly, these neeed to be removed
+# XXX It requires sudo because these will appear with `root` ownership
+rm -rf "$WORKSPACE"/{daemon,demo,base}
+
+bash "$WORKSPACE"/travis-builds/purge_cluster.sh
+# XXX purge_cluster only stops containers, it doesn't really remove them so try to
+# remove them for real
+containers_to_remove=$(docker ps -a -q)
+
+if [ "${containers_to_remove}" ]; then
+    docker rm -f $@ ${containers_to_remove} || echo failed to remove containers
+fi
+
+bash "$WORKSPACE"/travis-builds/build_imgs.sh
 
 # test
 #################################################################################
 
-# vars
-#################################################################################
-export ANSIBLE_SSH_ARGS=-F $CEPH_ANSIBLE_SCENARIO_PATH/vagrant_ssh_config
+# TODO: get the output image from build_imgs.sh to pass onto ceph-ansible
 
 # run vagrant and ceph-ansible tests
 #################################################################################
 cd "$CEPH_ANSIBLE_SCENARIO_PATH"
 vagrant up --no-provision --provider=$VAGRANT_PROVIDER
-bash $TOXINIDIR/tests/scripts/generate_ssh_config.sh $CEPH_ANSIBLE_SCENARIO_PATH
+
+bash $TOXINIDIR/ceph-ansible/tests/scripts/generate_ssh_config.sh $CEPH_ANSIBLE_SCENARIO_PATH
+
+export ANSIBLE_SSH_ARGS="-F $CEPH_ANSIBLE_SCENARIO_PATH/vagrant_ssh_config"
 
 ansible-playbook -vv -i $CEPH_ANSIBLE_SCENARIO_PATH/hosts $TOXINIDIR/ceph-ansible/site-docker.yml.sample --extra-vars="fetch_directory=$CEPH_ANSIBLE_SCENARIO_PATH/fetch"
 
