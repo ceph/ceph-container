03d28ea07f98c787c10a4767d6f8676bd452d5eb
Andrew Schoen <aschoen@redhat.com>
2017-01-12 16:36:13 -0600

    tests: adds a playbook to configure nodes to use insecure registries
    This will allow the test nodes to pull from any insecure registry that
    exists in the same subnet.
    
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- tests/tox.sh
+++ tests/tox.sh
@@ -52,6 +52,9 @@ bash $TOXINIDIR/ceph-ansible/tests/scripts/generate_ssh_config.sh $CEPH_ANSIBLE_
 
 export ANSIBLE_SSH_ARGS="-F $CEPH_ANSIBLE_SCENARIO_PATH/vagrant_ssh_config"
 
+
+# runs a playbook to configure nodes for testing
+ansible-playbook -vv -i $CEPH_ANSIBLE_SCENARIO_PATH/hosts $TOXINIDIR/tests/setup.yml
 ansible-playbook -vv -i $CEPH_ANSIBLE_SCENARIO_PATH/hosts $TOXINIDIR/ceph-ansible/site-docker.yml.sample --extra-vars="ceph_docker_dev_image=true fetch_directory=$CEPH_ANSIBLE_SCENARIO_PATH/fetch"
 
 ansible-playbook -vv -i $CEPH_ANSIBLE_SCENARIO_PATH/hosts $TOXINIDIR/ceph-ansible/tests/functional/setup.yml
