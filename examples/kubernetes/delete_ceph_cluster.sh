d24da88d8ebc01d4ae07e855c2f20cc2c6a80819
Ivan Font <ifont@redhat.com>
2017-01-23 12:53:34 -0800

    Documentation for Ceph cluster on GCE K8s cluster
    Adds documentation for setting up Ceph Kubernetes cluster on Google
    Compute Engine with steps on testing Rados Gateway S3 interface, CephFS,
    Ceph RBD, RBD and CephFS static provisioning of persistent volume
    claims, and lastly RBD dynamic provisioning of persistent volume claims.
    
    Adds new resources for demonstrating use of RBD dynamic provisioning of
    persistent volume claims.
    
    Updates RGW S3 client script to support RGW NodePort service type.
    
    Updates Ceph cluster delete script with extra steps to cleanup
    StorageClass and persistent volumes.

--- examples/kubernetes/delete_ceph_cluster.sh
+++ examples/kubernetes/delete_ceph_cluster.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 kubectl delete namespace ceph
+kubectl delete secret ceph-secret-admin --namespace kube-system
+kubectl delete storageclass slow
+kubectl delete pv --all
 kubectl label nodes --all node-type-
