6cb4b7d2febad1a47b87fb0fe53cf62a7031bbb3
Erwan Velu <erwan@redhat.com>
2017-02-20 18:31:52 +0100

    zap_device: declare complex device matching regexp once
    That very long matching string is kind of duplicated and complexify the
    code reading.
    
    This patch is just about defining it only once which is easier to
    maintain and makes the code easier to read.

--- ceph-releases/kraken/ubuntu/16.04/daemon/zap_device.sh
+++ ceph-releases/kraken/ubuntu/16.04/daemon/zap_device.sh
@@ -2,6 +2,8 @@
 set -e
 
 function zap_device {
+  local device_match_string='/dev/([hsv]d[a-z]{1,2}|cciss/c[0-9]d[0-9]p|nvme[0-9]n[0-9]p){1,2}'
+
   if [[ -z ${OSD_DEVICE} ]]; then
     log "Please provide device(s) to zap!"
     log "ie: '-e OSD_DEVICE=/dev/sdb' or '-e OSD_DEVICE=/dev/sdb,/dev/sdc'"
@@ -15,7 +17,7 @@ function zap_device {
       exit 1
     fi
     # if the disk passed is a raw device AND the boot system disk
-    if echo $device | egrep -sq '/dev/([hsv]d[a-z]{1,2}|cciss/c[0-9]d[0-9]p|nvme[0-9]n[0-9]p){1,2}$' && parted -s $(echo $device | egrep -o '/dev/([hsv]d[a-z]{1,2}|cciss/c[0-9]d[0-9]p|nvme[0-9]n[0-9]p){1,2}') print | grep -sq boot; then
+    if echo $device | egrep -sq ${device_match_string} && parted -s $(echo $device | egrep -o ${device_match_string}) print | grep -sq boot; then
       log "Looks like $device has a boot partition,"
       log "if you want to delete specific partitions point to the partition instead of the raw device"
       log "Do not use your system disk!"
@@ -33,8 +35,8 @@ function zap_device {
   fi
 
   for device in $(echo ${OSD_DEVICE} | tr "," " "); do
-    raw_device=$(echo $device | egrep -o '/dev/([hsv]d[a-z]{1,2}|cciss/c[0-9]d[0-9]p|nvme[0-9]n[0-9]p){1,2}')
-    if echo $device | egrep -sq '/dev/([hsv]d[a-z]{1,2}|cciss/c[0-9]d[0-9]p|nvme[0-9]n[0-9]p){1,2}$'; then
+    raw_device=$(echo $device | egrep -o ${device_match_string})
+    if echo $device | egrep -sq ${device_match_string}; then
       log "Zapping the entire device $device"
       sgdisk --zap-all --clear --mbrtogpt -g -- $device
     else
