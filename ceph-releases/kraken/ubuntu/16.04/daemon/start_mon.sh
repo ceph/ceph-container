4e4955e58b535d80a2b4ea5cc07007a6c46f2ae0
Erwan Velu <erwan@redhat.com>
2017-03-09 16:57:39 +0100

    start_mon: Let's use a loop to make the code smaller
    Less code, less bugs.

--- ceph-releases/kraken/ubuntu/16.04/daemon/start_mon.sh
+++ ceph-releases/kraken/ubuntu/16.04/daemon/start_mon.sh
@@ -120,9 +120,9 @@ function start_mon {
 
     # Testing if it's not the first monitor, if one key doesn't exist we assume none of them exist
     ceph-authtool /tmp/${CLUSTER}.mon.keyring --create-keyring --import-keyring /etc/ceph/${CLUSTER}.client.admin.keyring
-    ceph-authtool /tmp/${CLUSTER}.mon.keyring --import-keyring /var/lib/ceph/bootstrap-osd/${CLUSTER}.keyring
-    ceph-authtool /tmp/${CLUSTER}.mon.keyring --import-keyring /var/lib/ceph/bootstrap-mds/${CLUSTER}.keyring
-    ceph-authtool /tmp/${CLUSTER}.mon.keyring --import-keyring /var/lib/ceph/bootstrap-rgw/${CLUSTER}.keyring
+    for name in osd mds rgw; do
+      ceph-authtool /tmp/${CLUSTER}.mon.keyring --import-keyring /var/lib/ceph/bootstrap-$name/${CLUSTER}.keyring
+    done
     ceph-authtool /tmp/${CLUSTER}.mon.keyring --import-keyring /etc/ceph/${CLUSTER}.mon.keyring
     chown ceph. /tmp/${CLUSTER}.mon.keyring
 
