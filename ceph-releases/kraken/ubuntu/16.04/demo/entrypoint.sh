ff29d71eade0776e854482a945faf11c0b10cf3a
Sébastien Han <seb@redhat.com>
2017-03-29 17:39:37 +0200

    demo: use rgw name instead of localhost
    If we don't do this rgw can not translate requests.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- ceph-releases/kraken/ubuntu/16.04/demo/entrypoint.sh
+++ ceph-releases/kraken/ubuntu/16.04/demo/entrypoint.sh
@@ -336,8 +336,8 @@ function bootstrap_demo_user {
       echo "Secret key: $CEPH_DEMO_SECRET_KEY" >> /ceph-demo-user
 
       # Use rgw port
-      sed -i "s/host_base = localhost/host_base = localhost:${RGW_CIVETWEB_PORT}/" /root/.s3cfg
-      sed -i "s/host_bucket = localhost/host_bucket = localhost:${RGW_CIVETWEB_PORT}/" /root/.s3cfg
+      sed -i "s/host_base = localhost/host_base = ${RGW_NAME}:${RGW_CIVETWEB_PORT}/" /root/.s3cfg
+      sed -i "s/host_bucket = localhost/host_bucket = ${RGW_NAME}:${RGW_CIVETWEB_PORT}/" /root/.s3cfg
 
       if [ -n "$CEPH_DEMO_BUCKET" ]; then
         log "Creating bucket..."
