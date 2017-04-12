0f70502f456adf2a41308562e252343758e724b0
Bertrand Gouny <bertrand.gouny@osixia.net>
2017-04-12 09:26:10 +0200

    use /etc/ceph/.conf in config.k8s.sh get_mon_config()

--- ceph-releases/infernalis/ubuntu/14.04/daemon/config.k8s.sh
+++ ceph-releases/infernalis/ubuntu/14.04/daemon/config.k8s.sh
@@ -8,7 +8,7 @@ function get_admin_key {
 
 function get_mon_config {
   # Get FSID from ceph.conf
-  FSID=$(ceph-conf --lookup fsid -c /etc/ceph/ceph.conf)
+  FSID=$(ceph-conf --lookup fsid -c /etc/ceph/${CLUSTER}.conf)
 
   # Get the ceph mon pods (name and IP) from the Kubernetes API. Formatted as a set of monmap params
   MONMAP_ADD=$(kubectl get pods --namespace=${CLUSTER} -l daemon=mon -o template --template="{{range .items}}{{if .status.podIP}}--add {{.metadata.name}} {{.status.podIP}} {{end}} {{end}}")
@@ -21,4 +21,3 @@ function get_config {
    # No-op for static
    echo "k8s: config is stored as k8s secrets."
 }
-
