bca3bed36dcae7ffbdcd15f3e770df47d62d5665
ReSearchITEng <ReSearchITEng@users.noreply.github.com>
2017-05-10 11:20:08 +0300

    keeping jenkins' lint happy (previous code also)
    keeping jenkins' lint happy (previous code also)

--- examples/helm/build-all.sh
+++ examples/helm/build-all.sh
@@ -6,10 +6,10 @@ function list_dirs () {
 function build_charts () {
   echo "Building Charts"
   list_dirs | while read CHART; do
-    if [ -f ${CHART}/Makefile ]; then make -C ${CHART}; fi
-    if [ -f ${CHART}/requirements.yaml ]; then helm dep up ${CHART}; fi
-    if [ -f ${CHART}/Chart.yaml ]; then 
-      helm package ./${CHART}
+    if [ -f "${CHART}/Makefile" ]; then make -C ${CHART}; fi
+    if [ -f "${CHART}/requirements.yaml" ]; then helm dep up ${CHART}; fi
+    if [ -f "${CHART}/Chart.yaml" ]; then 
+      helm package "./${CHART}"
     fi
   done
 }
