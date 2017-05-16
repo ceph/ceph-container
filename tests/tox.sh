6c130375b1fe3a49032f2db48e79ab6acac195b1
Andrew Schoen <aschoen@redhat.com>
2017-05-16 11:36:25 -0500

    tests: also pass ceph_stable_release=$CEPH_RELEASE
    This ensures that the ceph-mgr role is skipped when deploying a jewel
    container.
    
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- tests/tox.sh
+++ tests/tox.sh
@@ -69,7 +69,7 @@ export ANSIBLE_SSH_ARGS="-F $CEPH_ANSIBLE_SCENARIO_PATH/vagrant_ssh_config"
 
 # runs a playbook to configure nodes for testing
 ansible-playbook -vv -i $CEPH_ANSIBLE_SCENARIO_PATH/hosts $TOXINIDIR/tests/setup.yml
-ansible-playbook -vv -i $CEPH_ANSIBLE_SCENARIO_PATH/hosts $TOXINIDIR/ceph-ansible/site-docker.yml.sample --extra-vars="ceph_docker_image_tag=$CEPH_RELEASE-latest ceph_docker_registry=$REGISTRY_ADDRESS fetch_directory=$CEPH_ANSIBLE_SCENARIO_PATH/fetch"
+ansible-playbook -vv -i $CEPH_ANSIBLE_SCENARIO_PATH/hosts $TOXINIDIR/ceph-ansible/site-docker.yml.sample --extra-vars="ceph_stable_release=$CEPH_RELEASE ceph_docker_image_tag=$CEPH_RELEASE-latest ceph_docker_registry=$REGISTRY_ADDRESS fetch_directory=$CEPH_ANSIBLE_SCENARIO_PATH/fetch"
 
 ansible-playbook -vv -i $CEPH_ANSIBLE_SCENARIO_PATH/hosts $TOXINIDIR/ceph-ansible/tests/functional/setup.yml
 
