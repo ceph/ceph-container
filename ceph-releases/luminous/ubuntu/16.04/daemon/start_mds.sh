985675e0583b6fc88f306f920b934fa6d89e5fc4
Sébastien Han <seb@redhat.com>
2017-09-09 01:07:37 +0200

    daemon: change MDS_NAME default to hostname
    The ms should never have existed from the beginning. We implemented a
    check within the mds code to ensure backward compatiblity with existing
    clusters.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- ceph-releases/luminous/ubuntu/16.04/daemon/start_mds.sh
+++ ceph-releases/luminous/ubuntu/16.04/daemon/start_mds.sh
@@ -5,24 +5,28 @@ function start_mds {
   get_config
   check_config
 
-  # Check to see if we are a new MDS
-  if [ ! -e "$MDS_KEYRING" ]; then
-    if [ -e "$ADMIN_KEYRING" ]; then
-      keyring_opt=(--name client.admin --keyring "$ADMIN_KEYRING")
-    elif [ -e "$MDS_BOOTSTRAP_KEYRING" ]; then
-      keyring_opt=(--name client.bootstrap-mds --keyring "$MDS_BOOTSTRAP_KEYRING")
-    else
-      log "ERROR- Failed to bootstrap MDS: could not find admin or bootstrap-mds keyring.  You can extract it from your current monitor by running 'ceph auth get client.bootstrap-mds -o $MDS_BOOTSTRAP_KEYRING"
-      exit 1
+  # The first check is a backward compatible check, the commit after f34ceff02553398814793cc939213222d95a475d
+  # broke the MDS_NAME variable for existing users. MDS_NAME was actually wrong from the beginning and the prefix
+  # should have never existed. This check verifies if a legacy MDS runs already, if so we don't do anything.
+  if [ ! -e /var/lib/ceph/mds/"${CLUSTER}"-mds-"${MDS_NAME}"/keyring ]; then
+    if [ ! -e "$MDS_KEYRING" ]; then
+      if [ -e "$ADMIN_KEYRING" ]; then
+        keyring_opt=(--name client.admin --keyring "$ADMIN_KEYRING")
+      elif [ -e "$MDS_BOOTSTRAP_KEYRING" ]; then
+        keyring_opt=(--name client.bootstrap-mds --keyring "$MDS_BOOTSTRAP_KEYRING")
+      else
+        log "ERROR- Failed to bootstrap MDS: could not find admin or bootstrap-mds keyring.  You can extract it from your current monitor by running 'ceph auth get client.bootstrap-mds -o $MDS_BOOTSTRAP_KEYRING"
+        exit 1
+      fi
+
+      timeout 10 ceph "${CLI_OPTS[@]}" "${keyring_opt[@]}" health || exit 1
+
+      # Generate the MDS key
+      ceph "${CLI_OPTS[@]}" "${keyring_opt[@]}" auth get-or-create mds."$MDS_NAME" osd 'allow rwx' mds 'allow' mon 'allow profile mds' -o "$MDS_KEYRING"
+      chown --verbose ceph. "$MDS_KEYRING"
+      chmod 600 "$MDS_KEYRING"
+
     fi
-
-    timeout 10 ceph "${CLI_OPTS[@]}" "${keyring_opt[@]}" health || exit 1
-
-    # Generate the MDS key
-    ceph "${CLI_OPTS[@]}" "${keyring_opt[@]}" auth get-or-create mds."$MDS_NAME" osd 'allow rwx' mds 'allow' mon 'allow profile mds' -o "$MDS_KEYRING"
-    chown --verbose ceph. "$MDS_KEYRING"
-    chmod 600 "$MDS_KEYRING"
-
   fi
 
   # NOTE (leseb): having the admin keyring is really a security issue
