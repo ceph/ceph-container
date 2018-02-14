b6cc0db85a7789aa4269e1e4673b66bf5fc50310
Blaine Gardner <blaine.gardner@suse.com>
2018-02-14 14:28:04 -0700

    Add build staging integration test from POC #899
    Based on the proof of concept from PR #899, add Python scripts to stage
    files for building.
    
    An integration test for this functionality can be found in
    `tests/stage-test` and can be run as `make stage-test` or manually as
    `./tests/stage-test/test_staging.sh`.
    
    Additionally add flake8 configuration to tox.ini for linting the added
    code. Currently, some of the existing code is also flagged by flake8, an
    area for future improvement. flake8 linting can be run with `make lint`.
    
    Signed-off-by: Blaine Gardner <blaine.gardner@suse.com>

---
+++ stage.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017 SUSE LLC
+
+import os
+import logging
+import shutil
+import sys
+import time
+
+from stagelib.envglobals import (printGlobal, CEPH_VERSION, OS_NAME, OS_VERSION,
+                                 IMAGES_TO_BUILD, STAGING_DIR)
+from stagelib.filetools import (list_files, mkdir_if_dne, copy_files, recursive_copy_dir,
+                                IOOSErrorGracefulFail)
+from stagelib.replace import do_variable_replace
+from stagelib.blacklist import get_blacklist
+
+
+# Set default values for tunables (primarily only interesting for testing)
+CORE_FILES_DIR = "src"
+CEPH_RELEASES_DIR = "ceph-releases"
+BLACKLIST_FILE = "flavor-blacklist.txt"
+LOG_FILE = os.path.join(STAGING_DIR, "stage.log")
+
+# Start with empty staging dir so there are no previous artifacts
+try:
+    if os.path.isdir(STAGING_DIR):
+        shutil.rmtree(STAGING_DIR)
+    os.makedirs(STAGING_DIR, mode=0o755)
+except (OSError, IOError) as o:
+    IOOSErrorGracefulFail(o,
+                          'Could not delete and recreate staging dir: {}'.format(STAGING_DIR))
+
+loglevel = logging.INFO
+# If DEBUG env var is set to anything (including empty string) except '0', log debug text
+if 'DEBUG' in os.environ and not os.environ['DEBUG'] == '0':
+    loglevel = logging.DEBUG
+logging.basicConfig(filename=LOG_FILE, level=loglevel,
+                    format='%(levelname)5s:  %(message)s')
+logger = logging.getLogger(__name__)
+
+# Build dependency on python3 for `replace.py`. Looking to py2.7 deprecation in 2020.
+if sys.version_info[0] < 3:
+    print('This must be run with Python 3+')
+    sys.exit(1)
+
+
+def main(CORE_FILES_DIR, CEPH_RELEASES_DIR, BLACKLIST_FILE):
+    logger.info('\n\n\n')  # Make it easier to determine where new runs start
+    logger.info('Start time: {}'.format(time.ctime()))
+
+    print('')
+    printGlobal('CEPH_VERSION')
+    printGlobal('OS_NAME')
+    printGlobal('OS_VERSION')
+    printGlobal('BASEOS_REG')
+    printGlobal('BASEOS_REPO')
+    printGlobal('BASEOS_TAG')
+    printGlobal('ARCH')
+    printGlobal('IMAGES_TO_BUILD')
+    printGlobal('STAGING_DIR')
+    print('')
+
+    # Search from least specfic to most specific
+    path_search_order = [
+        "{}".format(CORE_FILES_DIR),
+        os.path.join(CEPH_RELEASES_DIR, 'ALL'),
+        os.path.join(CEPH_RELEASES_DIR, 'ALL', OS_NAME),
+        os.path.join(CEPH_RELEASES_DIR, 'ALL', OS_NAME, OS_VERSION),
+        os.path.join(CEPH_RELEASES_DIR, CEPH_VERSION),
+        os.path.join(CEPH_RELEASES_DIR, CEPH_VERSION, OS_NAME),
+        os.path.join(CEPH_RELEASES_DIR, CEPH_VERSION, OS_NAME, OS_VERSION),
+    ]
+    logger.debug('Path search order: {}'.format(path_search_order))
+
+    blacklist = get_blacklist(BLACKLIST_FILE)
+    logger.debug('Blacklist: {}'.format(blacklist))
+
+    for image in IMAGES_TO_BUILD:
+        logger.info('')
+        logger.info('{}/'.format(image))
+        logger.info('    Copying files')
+        for src_path in path_search_order:
+            if not os.path.isdir(src_path):
+                continue
+            src_files = list_files(src_path)
+            # e.g., IMAGES_TO_BUILD = ['daemon-base', 'daemon']
+            staging_path = os.path.join(STAGING_DIR, image)
+            mkdir_if_dne(staging_path, mode=0o755)
+            copy_files(src_files, src_path, staging_path, blacklist)
+            recursive_copy_dir(src_path=os.path.join(src_path, image), dst_path=staging_path,
+                               blacklist=blacklist)
+        do_variable_replace(replace_root_dir=os.path.join(STAGING_DIR, image))
+
+
+if __name__ == "__main__":
+    main(CORE_FILES_DIR, CEPH_RELEASES_DIR, BLACKLIST_FILE)
