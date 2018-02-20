178efe7b22a8ef87d43edd79322d1e60e14136e2
Guillaume Abrioux <gabrioux@redhat.com>
2018-02-20 12:02:16 +0100

    short hostname shouldn't be only a default value
    `HOSTNAME` won't be set as expected since it's already defined when the
    containter starts.
    We must reset that variable to the value we actually want.
    
    Closes: https://bugzilla.redhat.com/show_bug.cgi?id=1546127
    
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

--- ceph-releases/jewel/ubuntu/14.04/daemon/variables_entrypoint.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/variables_entrypoint.sh
@@ -17,13 +17,10 @@ ALL_SCENARIOS="populate_kvstore mon osd osd_directory osd_directory_single osd_c
 : "${CEPH_CLUSTER_NETWORK:=${CEPH_PUBLIC_NETWORK}}"
 : "${CEPH_DAEMON:=${1}}" # default daemon to first argument
 : "${CEPH_GET_ADMIN_KEY:=0}"
-: "${HOSTNAME:=$(uname -n | cut -d'.' -f1)}"
-: "${MON_NAME:=${HOSTNAME}}"
 : "${MON_DATA_DIR:=/var/lib/ceph/mon/${CLUSTER}-${MON_NAME}}"
 : "${K8S_HOST_NETWORK:=0}"
 : "${K8S_MON_SELECTOR:=app=ceph,daemon=mon}"
 : "${NETWORK_AUTO_DETECT:=0}"
-: "${MDS_NAME:=${HOSTNAME}}"
 : "${OSD_FORCE_ZAP:=0}"
 : "${OSD_JOURNAL_SIZE:=100}"
 : "${OSD_DMCRYPT:=0}"
@@ -35,8 +32,6 @@ ALL_SCENARIOS="populate_kvstore mon osd osd_directory osd_directory_single osd_c
 : "${CEPHFS_DATA_POOL_PG:=8}"
 : "${CEPHFS_METADATA_POOL:=${CEPHFS_NAME}_metadata}"
 : "${CEPHFS_METADATA_POOL_PG:=8}"
-: "${RGW_NAME:=${HOSTNAME}}"
-: "${RBD_MIRROR_NAME:=${HOSTNAME}}"
 : "${RGW_ZONEGROUP:=}"
 : "${RGW_ZONE:=}"
 : "${RGW_CIVETWEB_IP:=0.0.0.0}"
@@ -53,6 +48,11 @@ ALL_SCENARIOS="populate_kvstore mon osd osd_directory osd_directory_single osd_c
 : "${KV_IP:=127.0.0.1}"
 : "${KV_PORT:=2379}"
 OSD_FILESTORE=1
+HOSTNAME=$(uname -n | cut -d'.' -f1)
+: "${MON_NAME:=${HOSTNAME}}"
+: "${RGW_NAME:=${HOSTNAME}}"
+: "${RBD_MIRROR_NAME:=${HOSTNAME}}"
+: "${MDS_NAME:=${HOSTNAME}}"
 
 # Create a default array
 CRUSH_LOCATION_DEFAULT=("root=default" "host=${HOSTNAME}")
