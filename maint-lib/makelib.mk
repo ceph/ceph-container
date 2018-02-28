6047c8a65b5dde5a7a2cb6d2e6771598c01ecd16
Blaine Gardner <blaine.gardner@suse.com>
2018-02-27 19:10:28 -0700

    Tidy up growing Makefile and root dir
    The makefile is starting to get quite big. Moving all "Internal
    definitions" except `ALL_BUILDABLE_FLAVORS` to a make library file
    helps reduce the amount of stuff by a couple dozen lines. Adding a
    make library file to the project root or to a new directory also
    adds one more thing to the project root which already feels full.
    
    Add a make library file in a new maintenance library dir called
    `maint-lib/`. At the same time, move `stage.py`,
    `flavors-modified-vs-master.py`, and the `stagelib/` dir to this
    maintenance library dir. Update the Makefile and the stage test
    script to support the dir changes.
    
    Signed-off-by: Blaine Gardner <blaine.gardner@suse.com>

---
+++ maint-lib/makelib.mk
@@ -0,0 +1,25 @@
+# Container images built for each flavor
+IMAGES_TO_BUILD := daemon-base daemon
+
+# Given a variable name $(1) and a build flavor $(2), print a string
+#   <variable name>="<variable value>". When used in a target, this will effectively set the
+#   environment variable <variable name> to the appropriate value.
+comma := ,
+define set_env_var
+$(shell set -eu ; \
+	CEPH_VERSION=$(word 1, $(subst $(comma), ,$(2))) ; \
+	ARCH=$(word 2, $(subst $(comma), ,$(2))) ; \
+	OS_NAME=$(word 3, $(subst $(comma), ,$(2))) ; \
+	OS_VERSION=$(word 4, $(subst $(comma), ,$(2))) ; \
+	BASEOS_REG=$(word 5, $(subst $(comma), ,$(2))) ; \
+	BASEOS_REPO=$(word 6, $(subst $(comma), ,$(2))) ; \
+	BASEOS_TAG=$(word 7, $(subst $(comma), ,$(2))) ; \
+	IMAGES_TO_BUILD='$(IMAGES_TO_BUILD)' ; \
+	STAGING_DIR=staging/$$CEPH_VERSION-$$BASEOS_REPO-$$BASEOS_TAG-$$ARCH ; \
+	BASE_IMAGE=$$BASEOS_REG/$$BASEOS_REPO:$$BASEOS_TAG ; \
+	BASE_IMAGE=$${BASE_IMAGE#_/} ; \
+	DAEMON_BASE_IMAGE=$(REGISTRY)/daemon-base:$$CEPH_VERSION-$$BASEOS_REPO-$$BASEOS_TAG-$$ARCH ; \
+	DAEMON_IMAGE=$(REGISTRY)/daemon:$$CEPH_VERSION-$$BASEOS_REPO-$$BASEOS_TAG-$$ARCH ; \
+	echo "$(1)=\"$$$(1)\""
+)
+endef
