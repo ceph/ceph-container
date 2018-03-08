5cb92b80b3ebec945af1ec23002da4bba9183468
Blaine Gardner <blaine.gardner@suse.com>
2018-03-08 15:39:46 -0700

    staging: update tests
    Add newly required env vars to stage testing since it's now broken.
    
    With the refactor to verifying environment variables, also test the
    output of staging since it prints the required env vars. This is a
    concise, effective test that makes sure the env vars are being imported
    correctly.
    
    Add test for computed git env vars
    
    Signed-off-by: Blaine Gardner <blaine.gardner@suse.com>

--- tests/stage-test/test_staging.sh
+++ tests/stage-test/test_staging.sh
@@ -12,11 +12,15 @@ export BASEOS_REPO=ubuntu
 export BASEOS_TAG=16.04
 export STAGING_DIR=tests/stage-test/staging/${CEPH_VERSION}-${BASEOS_REPO}-${BASEOS_TAG}-${ARCH}
 export IMAGES_TO_BUILD="daemon-base daemon"
+export RELEASE='test-release'
+export DAEMON_BASE_IMAGE=test-reg/daemon-base:test-release-1
+export DAEMON_IMAGE=test-reg/daemon:test-release-1
 
 run_stage=$(cat <<'EOF'
 import sys
 sys.path.append('maint-lib')
 from stage import *
+import stagelib.git as git
 
 CORE_FILES_DIR = "tests/stage-test/src"
 CEPH_RELEASES_DIR = "tests/stage-test/ceph-releases/"
@@ -31,11 +35,15 @@ git.get_hash = get_hash
 def file_is_dirty(file):
     return True if file == 'tests/stage-test/src/daemon/src-daemon-test-file' else False
 git.file_is_dirty = file_is_dirty
+def branch_is_dirty():
+    return True
+git.branch_is_dirty = branch_is_dirty
 
 main(CORE_FILES_DIR, CEPH_RELEASES_DIR, BLACKLIST_FILE)
 EOF
 )
-python3 -c "${run_stage}"
+python3 -c "${run_stage}" | tee tests/stage-test/staging_output.txt
+mv tests/stage-test/staging_output.txt ${STAGING_DIR}
 
 diff --brief -Nr --exclude 'find-src' --exclude '*.log' tests/stage-test/stage-key/ "${STAGING_DIR}"
 
