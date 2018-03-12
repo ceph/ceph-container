9efa2261114ff80d8377da85cfc980b7a04e9fbf
Sébastien Han <seb@redhat.com>
2018-03-12 17:58:39 +0100

    travis: fix demo boostrap and validation
    Add better checks for rbd mirror, rgw and mgr
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- travis-builds/validate_demo_cluster.sh
+++ travis-builds/validate_demo_cluster.sh
@@ -2,7 +2,9 @@
 set -xe
 
 
-# FUNCTIONS
+#############
+# FUNCTIONS #
+#############
 function get_cluster_name {
   cluster=$(docker exec ceph-demo grep -R fsid /etc/ceph/ | egrep -o '^[^.]*')
   DOCKER_COMMAND="docker exec ceph-demo ceph --cluster $(basename $cluster)"
@@ -22,24 +24,29 @@ function wait_for_daemon () {
 }
 
 function get_ceph_version {
+  # shellcheck disable=SC2046
   echo $($DOCKER_COMMAND --version | grep -Eo '[0-9][0-9]\.[0-9]')
 }
 
 function ceph_status {
   echo "Waiting for Ceph to be ready"
+  # shellcheck disable=SC2046
   return $(wait_for_daemon "$DOCKER_COMMAND health | grep -sq HEALTH_OK")
 }
 
 function test_demo_mon {
+  # shellcheck disable=SC2046
   return $(wait_for_daemon "$DOCKER_COMMAND -s | grep -sq quorum")
 }
 
 function test_demo_osd {
+  # shellcheck disable=SC2046
   return $(wait_for_daemon "$DOCKER_COMMAND -s | grep -sq '1 osds: 1 up, 1 in'")
 }
 
 function test_demo_rgw {
-  return $(wait_for_daemon "$DOCKER_COMMAND osd dump | grep -sq rgw")
+  # shellcheck disable=SC2046
+  return $(wait_for_daemon "$DOCKER_COMMAND -s | grep -sq 'rgw:'")
 }
 
 function test_demo_mds {
@@ -47,18 +54,24 @@ function test_demo_mds {
   # NOTE(leseb): metadata server always takes up to 5 sec to run
   # so we first check if the pools exit, from that we assume that
   # the process will start. We stop waiting after 10 seconds.
+  # shellcheck disable=SC2046
   return $(wait_for_daemon "$DOCKER_COMMAND osd dump | grep -sq cephfs && $DOCKER_COMMAND -s | grep -sq 'up:active'")
 }
 
 function test_demo_rbd_mirror {
-  docker exec ceph-demo ps aux | grep -sq [r]bd-mirror
+  # shellcheck disable=SC2046
+  return $(wait_for_daemon "$DOCKER_COMMAND -s | grep -sq 'rbd-mirror:'")
 }
 
 function test_demo_mgr {
-  return $(wait_for_daemon "$DOCKER_COMMAND -s | grep -sq 'mgr active:'")
+  # shellcheck disable=SC2046
+  return $(wait_for_daemon "$DOCKER_COMMAND -s | grep -sq 'mgr:'")
 }
 
-# MAIN
+
+########
+# MAIN #
+########
 get_cluster_name
 ceph_status # wait for the cluster to stabilize
 test_demo_mon
