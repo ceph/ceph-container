5daf9478c1617a48d876a09d667cf41b5d557919
Sébastien Han <seb@redhat.com>
2018-03-13 00:01:02 +0100

    ci: refactor with new repo structure
    With the new repo structure and method to build images we have to change
    how the CI operates for newly created PRs.
    The workflow remains the same, if you submit a PR, we look at what has
    changed and we build the right image for it.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- tests/tox.sh
+++ tests/tox.sh
@@ -24,23 +24,29 @@ EOF
 
 # Check distro and install deps
 if command -v apt-get &>/dev/null; then
-    sudo apt-get install -y --force-yes docker.io
-    sudo apt-get install -y --force-yes xfsprogs
-  else
-    sudo yum install -y docker
-    sudo yum install -y xfsprogs
-    if ! systemctl status docker >/dev/null; then
-      # daemon doesn't start automatically after being installed
-      sudo systemctl restart docker
-    fi
-    # Allow running `docker` without sudo
-    sudo chgrp "$(whoami)" /var/run/docker.sock
+  sudo apt-get install -y --force-yes docker.io xfsprogs python3.6
+  sudo ln -sf "$(command -v python3.6)" /usr/bin/python3
+else
+  sudo yum install -y docker xfsprogs
+  if ! command -v python3.6 &>/dev/null; then
+    sudo yum -y groupinstall development
+    sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+    sudo yum -y install python36u
+  fi
+  sudo ln -sf "$(command -v python3.6)" /usr/bin/python3
+
+  if ! systemctl status docker >/dev/null; then
+    # daemon doesn't start automatically after being installed
+    sudo systemctl restart docker
+  fi
+  # Allow running `docker` without sudo
+  sudo chgrp "$(whoami)" /var/run/docker.sock
 fi
 
 rm -rf "$WORKSPACE"/ceph-ansible || true
 git clone -b $CEPH_ANSIBLE_BRANCH --single-branch https://github.com/ceph/ceph-ansible.git ceph-ansible
 
-if [ "$CEPH_ANSIBLE_BRANCH" == 'stable-2.2' -o "$CEPH_ANSIBLE_BRANCH" == 'stable-3.0' ]; then
+if [[ "$CEPH_ANSIBLE_BRANCH" == 'stable-2.2' ]] || [[ "$CEPH_ANSIBLE_BRANCH" == 'stable-3.0' ]]; then
   REQUIREMENTS=requirements2.2.txt
 else
   REQUIREMENTS=requirements2.4.txt
@@ -48,46 +54,60 @@ fi
 
 pip install -r $TOXINIDIR/ceph-ansible/tests/$REQUIREMENTS
 
-# pull requests tests should never have these directories here, but branches
-# do, so for the build scripts to work correctly, these neeed to be removed
-# XXX It requires sudo because these will appear with `root` ownership
-rm -rf "$WORKSPACE"/{daemon,demo,base}
-
 bash "$WORKSPACE"/travis-builds/purge_cluster.sh
 # XXX purge_cluster only stops containers, it doesn't really remove them so try to
 # remove them for real
 containers_to_remove=$(sudo docker ps -a -q)
 
 if [ "${containers_to_remove}" ]; then
-    sudo docker rm -f $@ ${containers_to_remove} || echo failed to remove containers
+  sudo docker rm -f "$@" ${containers_to_remove} || echo failed to remove containers
 fi
 
-# copy the files to the root for the
-# types of images we're going to build
-mkdir -p {base,daemon,demo}
-# starting with kraken, the base image does not exist
-cp -Lrv ceph-releases/$CEPH_STABLE_RELEASE/$IMAGE_DISTRO/base/* base || true
-cp -Lrv ceph-releases/$CEPH_STABLE_RELEASE/$IMAGE_DISTRO/daemon/* daemon
-cp -Lrv ceph-releases/$CEPH_STABLE_RELEASE/$IMAGE_DISTRO/demo/* demo || true
+cd "$WORKSPACE"
 
-bash "$WORKSPACE"/travis-builds/build_imgs.sh
+# build everything that was touched to make sure build succeeds
+FLAVOR_ARRAY=( $(make flavors.modified) )
+
+if [[ "${#FLAVOR_ARRAY[@]}" -eq "0" ]]; then
+  echo "The ceph-container code has not changed."
+  echo "Nothing to test here."
+  echo "SUCCESS"
+  exit 0
+fi
+
+if [[ "${#FLAVOR_ARRAY[@]}" -eq "1" ]]; then
+  FLAVOR="${FLAVOR_ARRAY[0]}"
+else
+  # if more than one release/distro is impacted then we test this in priority
+  FLAVOR="luminous,amd64,ubuntu,16.04,_,ubuntu,16.04"
+fi
+
+CURRENT_CEPH_STABLE_RELEASE="$(echo $FLAVOR|awk -F ',' '{ print $1}')"
+
+# CEPH_STABLE_RELEASE is an info passed by the CI (see tox.ini)
+# if CEPH_STABLE_RELEASE does not match CURRENT_CEPH_STABLE_RELEASE then CEPH_STABLE_RELEASE wins
+# so we will build the desired CEPH_STABLE_RELEASE since the current patch didn't change the ceph version
+# Since we test all the Ceph releases, we will always test the impacted one
+if [[ "$CEPH_STABLE_RELEASE" != "$CURRENT_CEPH_STABLE_RELEASE" ]]; then
+  FLAVOR="$CEPH_STABLE_RELEASE,amd64,ubuntu,16.04,_,ubuntu,16.04"
+fi
+
+echo "Building flavor $FLAVOR"
+sudo make FLAVORS="$FLAVOR" build.parallel
+
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
+# build tag from luminous,amd64,centos,7,_,centos,7 to luminous-centos-7-amd64
+TAG="${current_branch}-$(echo $FLAVOR|awk -F ',' '{ print $1"-"$3"-"$4"-"$2}')"
 
 # start a local docker registry
 sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2
 # add the image we just built to the registry
-sudo docker tag ceph/daemon localhost:5000/ceph/daemon:$CEPH_STABLE_RELEASE-latest
+sudo docker tag ceph/daemon:$TAG localhost:5000/ceph/daemon:$CEPH_STABLE_RELEASE-latest
 # this avoids a race condition between the tagging and the push
 # which causes this to sometimes fail when run by jenkins
 sleep 1
 sudo docker --debug push localhost:5000/ceph/daemon:$CEPH_STABLE_RELEASE-latest
 
-# test
-#################################################################################
-
-# TODO: get the output image from build_imgs.sh to pass onto ceph-ansible
-
-# run vagrant and ceph-ansible tests
-#################################################################################
 cd "$CEPH_ANSIBLE_SCENARIO_PATH"
 vagrant up --no-provision --provider=$VAGRANT_PROVIDER
 
