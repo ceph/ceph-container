ea24e23501910be77a2a6f8cd0be9703c5f1b1ff
Sébastien Han <seb@redhat.com>
2018-03-22 11:57:41 +0800

    daemon: add support for iscsi scenarios
    We now have 3 new scenarios:
    
    * tcmu-runner
    * start_rbd_target_api
    * start_rbd_target_gw
    
    They help us deploying ceph-iscsi gateways.
    
    Currently, packages for ceph-iscsi are only built on Red Hat systems.
    This is why activation of one of these scenario will fail if the OS of
    the container image is not Red Hat or CentOS.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

---
+++ src/daemon/start_tcmu_runner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+function start_tcmu_runner {
+  get_config
+  check_config
+
+  if [ "${CEPH_GET_ADMIN_KEY}" -eq 1 ]; then
+    # ensure we have the admin key
+    get_admin_key
+    check_admin_key
+  fi
+
+  ceph_health client.admin /etc/ceph/"$CLUSTER".client.admin.keyring
+
+  log "SUCCESS"
+  # start tcmu-runner
+  exec tcmu-runner
+}
