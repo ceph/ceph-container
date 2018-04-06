4d88421a2463a538afb3a35e4619c1c53a4f28f7
Erwan Velu <erwan@redhat.com>
2018-04-06 12:22:16 +0200

    python: Fixing E251
    examples/kubernetes/rgw_s3_client.py was featuring many E251 generating
    useless flake8 errors.
    
    Signed-off-by: Erwan Velu <evelu@redhat.com>

--- examples/kubernetes/rgw_s3_client.py
+++ examples/kubernetes/rgw_s3_client.py
@@ -7,20 +7,20 @@ access_key = 'XXXXXXXXXXXXXXXXXXXX'
 secret_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 conn = boto.connect_s3(
-    aws_access_key_id = access_key,
-    aws_secret_access_key = secret_key,
-    host = '0.0.0.0',
-    port = None,     # Leave as None to use default port 80 or 443
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    host='0.0.0.0',
+    port=None,     # Leave as None to use default port 80 or 443
     is_secure=False, # comment out or set to True if you are using ssl
-    calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+    calling_format=boto.s3.connection.OrdinaryCallingFormat(),
 )
 
 bucket = conn.create_bucket('my-s3-test-bucket')
 
 for bucket in conn.get_all_buckets():
     print "{name}\t{created}".format(
-        name = bucket.name,
-        created = bucket.creation_date,
+        name=bucket.name,
+        created=bucket.creation_date,
     )
 
 key = bucket.new_key('hello.txt')
