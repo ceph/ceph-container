a33dc3ec161771ca72d9846a5907fc58d469f804
Erwan Velu <erwan@redhat.com>
2018-04-17 18:00:40 +0200

    ceph-releases/rhel7: Adding compose script
    The build mecanism for rhel is a bit specific and requires for now a
    single docker file for the whole thing.
    
    This generates an issue with the current project architecture as the
    content is split across two stages (daemon/daemon-base).
    
    This patch is about putting all the logic in daemon-base and copy the
    remaining files & the end of the docker file from the daemon/ into a
    composed directory.
    
    The preparation of this directory is done via the
    contrib/compose-rhcs.sh script. This is not really statisfying but will
    work a workaround for a short.
    
    Signed-off-by: Erwan Velu <evelu@redhat.com>

---
+++ contrib/compose-rhcs.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+set -e
+
+
+#############
+# VARIABLES #
+#############
+
+STAGING_DIR=staging/luminous-rhel7-7-released-x86_64/
+DAEMON_DIR=$STAGING_DIR/daemon
+DAEMON_BASE_DIR=${DAEMON_DIR}-base/
+DOCKERFILE_DAEMON=$DAEMON_DIR/Dockerfile
+DOCKERFILE_DAEMON_BASE=$DAEMON_DIR/Dockerfile
+COMPOSED_DIR=$STAGING_DIR/composed
+
+
+#############
+# FUNCTIONS #
+#############
+
+fatal() {
+  echo "FATAL ERROR !"
+  echo "########################################################"
+  echo "$@"
+  echo "########################################################"
+  exit 1
+}
+
+check_staging_exist(){
+  for dockerfile in "$DOCKERFILE_DAEMON" "$DOCKERFILE_DAEMON_BASE"; do
+    if [ ! -f "$dockerfile" ]; then
+      fatal "Missing dockerfile $dockerfile ! Please stage first !"
+    fi
+  done
+}
+
+create_compose_directory() {
+  if [ -d "$COMPOSED_DIR" ]; then
+    rm -rf "${COMPOSED_DIR:?}"
+  fi
+  mkdir -p $COMPOSED_DIR
+}
+
+import_content() {
+  rsync -a --exclude "__*__" --exclude "*.bak" --exclude "*.md" "$1"/* $COMPOSED_DIR/ || fatal "Cannot rsync"
+}
+
+# Select the end of the daemon Dockerfile to complete the daemon-base's one
+merge_content() {
+  grep -B1 -A1000 "# Add ceph-container files" $DOCKERFILE_DAEMON >> $COMPOSED_DIR/Dockerfile || fatal "Cannot find starting point in $DOCKERFILE_DAEMON"
+}
+
+clean_staging() {
+  if [ -d "$STAGING_DIR" ]; then
+    rm -rf "${STAGING_DIR:?}"
+  fi
+}
+
+make_staging() {
+  make FLAVORS=luminous,rhel7,7-released || fatal "Cannot build rhel7"
+}
+
+success() {
+  echo "###########################################################################################"
+  echo "Composed RHCS directory is available at $COMPOSED_DIR"
+  echo "###########################################################################################"
+}
+
+
+########
+# MAIN #
+########
+
+clean_staging
+make_staging
+check_staging_exist
+create_compose_directory
+import_content $DAEMON_DIR
+import_content $DAEMON_BASE_DIR
+merge_content
+success
