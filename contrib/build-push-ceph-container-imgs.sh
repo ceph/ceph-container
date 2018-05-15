b5d25055a19d578c8b976f3f66f4fde0956328e3
Sébastien Han <seb@redhat.com>
2018-05-15 11:25:40 +0200

    contrib: bump docker version
    We need to use the lastest stable release so we can get the 'manifest'
    subcommand from the CLI.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- contrib/build-push-ceph-container-imgs.sh
+++ contrib/build-push-ceph-container-imgs.sh
@@ -22,8 +22,12 @@ function cleanup_previous_run {
 }
 
 function install_docker {
-  sudo apt-get install -y --force-yes docker.io
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get install -y --force-yes docker-ce
   sudo systemctl start docker
+  sudo systemctl status docker
   sudo chgrp "$(whoami)" /var/run/docker.sock
 }
 
