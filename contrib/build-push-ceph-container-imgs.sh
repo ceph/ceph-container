f0536ae9f406a24b9c975d046c4e4c7ac2c0fdbc
Sébastien Han <seb@redhat.com>
2018-05-15 19:51:27 +0200

    contrib: arm64 install docker manually
    We do not execute the docker manifest command on this machine so there
    is no need for a newer docker version.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- contrib/build-push-ceph-container-imgs.sh
+++ contrib/build-push-ceph-container-imgs.sh
@@ -22,6 +22,7 @@ function cleanup_previous_run {
   make clean.all || true
 }
 
+declare -F install_docker ||
 function install_docker {
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
