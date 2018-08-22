d0c70a4350fe17f1ceec7e828c77d12fa7092c33
David Galloway <dgallowa@redhat.com>
2018-08-22 16:53:06 -0400

    tests: set owner of staging dir to jenkins user
    Fixes: https://github.com/ceph/ceph-container/issues/1048
    
    Signed-off-by: David Galloway <dgallowa@redhat.com>

--- tests/teardown.sh
+++ tests/teardown.sh
@@ -4,4 +4,6 @@
 . "$WORKSPACE/.tox_vars"
 cd "$CEPH_ANSIBLE_SCENARIO_PATH" || exit 1
 vagrant destroy --force
+# see https://github.com/ceph/ceph-container/issues/1048
+sudo chown -R "$(whoami)" ./staging || true
 cd "$WORKSPACE" || exit
