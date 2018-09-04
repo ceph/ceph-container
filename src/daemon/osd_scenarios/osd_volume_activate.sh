e5297bcfbff177edbab43227773230cfe7cd6ee7
Erwan Velu <erwan@redhat.com>
2018-09-04 15:28:42 +0200

    osd: Reporting more context when mounting fails
    When sigterm_post_cleanup fails to umount the OSD directory a little bit of context is missing :
    - what was the calling function
    - what was the process preventing the umount
    
    This patch is adding this missing context to help debugging.
    
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1624341
    
    Signed-off-by: Erwan Velu <evelu@redhat.com>

--- src/daemon/osd_scenarios/osd_volume_activate.sh
+++ src/daemon/osd_scenarios/osd_volume_activate.sh
@@ -29,8 +29,8 @@ function osd_volume_activate {
     local ceph_mnt
     ceph_mnt=$(findmnt --nofsroot --noheadings --output SOURCE --submounts --target /var/lib/ceph/osd/ | tail -n +2)
     for mnt in $ceph_mnt; do
-      log "Unmounting $mnt"
-      umount "$mnt" || log "Failed to umount $mnt"
+      log "osd_volume_activate: Unmounting $mnt"
+      umount "$mnt" || (log "osd_volume_activate: Failed to umount $mnt"; lsof $mnt)
     done
   }
   exec /usr/bin/ceph-osd "${CLI_OPTS[@]}" -f -i "${OSD_ID}"
