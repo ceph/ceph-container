9cd66e3e571bded14b3ae138bdc128fc94b46697
Sébastien Han <seb@redhat.com>
2018-09-28 16:12:26 +0200

    osd: discover osd objecstore before activation
    When activating a device via ceph-volume we need to pass the objectstore
    since it can be different than the default 'bluestore', so let's
    discover it first. The variables OSD_FILESTORE and OSD_BLUESTORE are
    typically passed by Ansible.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- src/daemon/osd_scenarios/osd_volume_activate.sh
+++ src/daemon/osd_scenarios/osd_volume_activate.sh
@@ -17,8 +17,22 @@ function osd_volume_activate {
   # Find the OSD FSID
   OSD_FSID="$(ceph-volume lvm list "$OSD_DEVICE" --format json | python -c "import sys, json; print(json.load(sys.stdin)[\"$OSD_ID\"][0][\"tags\"][\"ceph.osd_fsid\"])")"
 
+  # Discover the objectstore
+  if [[ "OSD_FILESTORE" -eq 1 ]]; then
+    OSD_OBJECTSTORE=(--filestore)
+  elif [[ "OSD_BLUESTORE" -eq 1 ]]; then
+    OSD_OBJECTSTORE=(--bluestore)
+  else
+    log "Either OSD_FILESTORE or OSD_BLUESTORE must be set to 1."
+    exit 1
+  fi
+
   # Activate the OSD
-  ceph-volume lvm activate --no-systemd "${OSD_ID}" "${OSD_FSID}"
+  # The command can fail so if it does, let's output the ceph-volume logs
+  if ! ceph-volume lvm activate --no-systemd "${OSD_OBJECTSTORE[@]}" "${OSD_ID}" "${OSD_FSID}"; then
+    cat /var/log/ceph
+    exit 1
+  fi
 
   log "SUCCESS"
   # This ensures all resources have been unmounted after the OSD has exited
