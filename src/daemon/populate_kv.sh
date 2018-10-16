ceph/ceph-container

commit 32b1feca851ac8fba9f60f3848e85c0503fd6ead

Create /ceph-config/ceph/client_host automatically
If anybody executes properly the `populate_kvstore` script before booting the cluster, it is expected that it works.

Currently, however, monitor daemons fail with:

    ERROR 100: Key not found (/ceph-config/ceph/client_host)

This patch fixes #901.

Signed-off-by: Jairo Llopis <yajo.sk8@gmail.com>

src/daemon/populate_kv.sh
@@ -8,6 +8,7 @@ function kv {
   read -r key value <<< "$*"
   log "Adding key ${key} with value ${value} to KV store."
   etcdctl "${ETCDCTL_OPTS[@]}" "${KV_TLS[@]}" set "${CLUSTER_PATH}""${key}" "${value}" || log "Value is already set"
+  etcdctl "${ETCDCTL_OPTS[@]}" "${KV_TLS[@]}" mkdir "${CLUSTER_PATH}client_host" || log "client_host already exists"
 }
 
 function populate_kv {

commit ca7c6ace240a12018fbaeb8d6dabf3c5f0f54360

Fix typo when creating client_host
Typo introduced in https://github.com/ceph/ceph-container/pull/1186, that was making it not actually fixing #901.

Signed-off-by: Jairo Llopis <yajo.sk8@gmail.com>

src/daemon/populate_kv.sh
@@ -8,7 +8,7 @@ function kv {
   read -r key value <<< "$*"
   log "Adding key ${key} with value ${value} to KV store."
   etcdctl "${ETCDCTL_OPTS[@]}" "${KV_TLS[@]}" set "${CLUSTER_PATH}""${key}" "${value}" || log "Value is already set"
-  etcdctl "${ETCDCTL_OPTS[@]}" "${KV_TLS[@]}" mkdir "${CLUSTER_PATH}client_host" || log "client_host already exists"
+  etcdctl "${ETCDCTL_OPTS[@]}" "${KV_TLS[@]}" mkdir "${CLUSTER_PATH}/client_host" || log "client_host already exists"
 }
 
 function populate_kv {