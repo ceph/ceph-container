5502fd7fdb13d89642a4312def7e2f457a3c49a0
Alfredo Deza <adeza@redhat.com>
2019-02-12 15:04:27 -0500

    contrib: set noninteractive for apt-get
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- contrib/build-push-ceph-container-imgs.sh
+++ contrib/build-push-ceph-container-imgs.sh
@@ -32,7 +32,7 @@ function install_docker {
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   sudo apt-get update
-  sudo apt-get install -y --force-yes docker-ce
+  sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes docker-ce
   sudo systemctl start docker
   sudo systemctl status docker
   sudo chgrp "$(whoami)" /var/run/docker.sock
