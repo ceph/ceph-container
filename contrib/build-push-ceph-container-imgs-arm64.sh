83ab8f917ff333268bb10d049061c86fbba2a2cb
Alfredo Deza <adeza@redhat.com>
2019-02-12 15:04:06 -0500

    contrib: set noninteractive for apt-get in arm64
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- contrib/build-push-ceph-container-imgs-arm64.sh
+++ contrib/build-push-ceph-container-imgs-arm64.sh
@@ -13,7 +13,7 @@ CEPH_RELEASES=(luminous mimic)
 #############
 
 function install_docker {
-  sudo apt-get install -y --force-yes docker.io
+  sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes docker.io
   sudo systemctl start docker
   sudo systemctl status docker
   sudo chgrp "$(whoami)" /var/run/docker.sock
