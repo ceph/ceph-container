778c7f91f0ccf325aae783020eb1f35a451231da
Dimitri Savineau <dsavinea@redhat.com>
2019-03-19 14:32:36 -0400

    daemon: Add support for python3
    Currently some entrypoint scripts were using python command. This
    will in most of case refers to python 2 and not python 3.
    Instead of hardcoding the python binary in the shell scripts we can
    test if the binary exist and set the PYTHON variable.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1690093
    
    Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>

--- src/daemon/watch_mgr_health.sh
+++ src/daemon/watch_mgr_health.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-IS_MGR_AVAIL=$(ceph "${CLI_OPTS[@]}" mgr dump | python -c "import json, sys; print json.load(sys.stdin)['available']")
+IS_MGR_AVAIL=$(ceph "${CLI_OPTS[@]}" mgr dump | $PYTHON -c "import json, sys; print(json.load(sys.stdin)['available'])")
 if [ "${IS_MGR_AVAIL}" == "True" ]; then
   exit 0
 else
