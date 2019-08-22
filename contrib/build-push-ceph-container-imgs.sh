9b54c3630d32ac283f0c9bfaea85ee3b3c76c919
Dan Mick <dan.mick@redhat.com>
2019-08-21 17:30:20 -0700

    Change install_docker to allow builds on a yum-based machine.
    Signed-off-by: Dan Mick <dan.mick@redhat.com>

--- contrib/build-push-ceph-container-imgs.sh
+++ contrib/build-push-ceph-container-imgs.sh
@@ -59,10 +59,14 @@ function cleanup_previous_run {
 
 declare -F install_docker ||
 function install_docker {
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  sudo apt-get update
-  sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes docker-ce
+  if [[ -x /usr/bin/yum ]] ; then
+    sudo yum install -y docker
+  else
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes docker-ce
+  fi
   sudo systemctl start docker
   sudo systemctl status docker
   sudo chgrp "$(whoami)" /var/run/docker.sock
