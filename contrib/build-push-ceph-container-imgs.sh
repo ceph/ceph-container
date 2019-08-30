ceph/ceph-container

commit d4eedeb870355f41ca9690cdd4047356ae7ffc12

Clarify variables to distinguish ceph-container.git from ceph.git
We'll be introducing a Ceph branch/sha1 pair soon

Signed-off-by: Dan Mick <dan.mick@redhat.com>

contrib/build-push-ceph-container-imgs.sh
@@ -7,8 +7,8 @@ set -ex
 #############
 
 # GIT_BRANCH is typically 'origin/master', we strip the variable to only get 'master'
-BRANCH="${GIT_BRANCH#*/}"
-LATEST_COMMIT_SHA=$(git rev-parse --short HEAD)
+CONTAINER_BRANCH="${GIT_BRANCH#*/}"
+CONTAINER_SHA=$(git rev-parse --short HEAD)
 TAGGED_HEAD=false # does HEAD is on a tag ?
 DEVEL=${DEVEL:=false}
 if [ -z "$CEPH_RELEASES" ]; then
@@ -122,19 +122,19 @@ function create_head_or_point_release {
 
     # find branch associated to that tag
     BRANCH=$(git branch -r --contains tags/"${tag_to_build[*]}" | grep -Eo 'stable-[0-9].[0-9]')
-    echo "Building a release Ceph container image based on branch $BRANCH and tag ${tag_to_build[*]}"
-    RELEASE="${tag_to_build[*]}-$BRANCH"
+    echo "Building a release Ceph container image based on branch $CONTAINER_BRANCH and tag ${tag_to_build[*]}"
+    RELEASE="${tag_to_build[*]}-$CONTAINER_BRANCH"
     # (todo): remove this when we have a better solution like running
     # the build script directly from the right branch.
-    if [ "${BRANCH}" == "stable-3.2" ]; then
+    if [ "${CONTAINER_BRANCH}" == "stable-3.2" ]; then
       CEPH_RELEASES=(luminous mimic)
-    elif [ "${BRANCH}" == "stable-4.0" ]; then
+    elif [ "${CONTAINER_BRANCH}" == "stable-4.0" ]; then
       CEPH_RELEASES=(nautilus)
     fi
   else
     set -e
-    echo "Building a devel Ceph container image based on branch $BRANCH and commit $LATEST_COMMIT_SHA"
-    RELEASE="$BRANCH-$LATEST_COMMIT_SHA"
+    echo "Building a devel Ceph container image based on branch $CONTAINER_BRANCH and commit $CONTAINER_SHA"
+    RELEASE="$CONTAINER_BRANCH-$CONTAINER_SHA"
   fi
 }
 
@@ -156,14 +156,14 @@ function build_and_push_latest_bis {
   # latest-bis-$ceph_release is needed by ceph-ansible so it can test the restart handlers on an image ID change
   # rebuild latest again to get a different image ID
   for ceph_release in "${CEPH_RELEASES[@]}"; do
-    make RELEASE="$BRANCH"-bis FLAVORS="${ceph_release}",centos,7 build
-    docker tag ceph/daemon:"$BRANCH"-bis-"${ceph_release}"-centos-7-"${HOST_ARCH}" ceph/daemon:latest-bis-"$ceph_release"
+    make RELEASE="$CONTAINER_BRANCH"-bis FLAVORS="${ceph_release}",centos,7 build
+    docker tag ceph/daemon:"$CONTAINER_BRANCH"-bis-"${ceph_release}"-centos-7-"${HOST_ARCH}" ceph/daemon:latest-bis-"$ceph_release"
     docker push ceph/daemon:latest-bis-"$ceph_release"
   done
 
   # Now let's build the latest
-  make RELEASE="$BRANCH"-bis FLAVORS="${CEPH_RELEASES[-1]}",centos,7 build
-  docker tag ceph/daemon:"$BRANCH"-bis-"${CEPH_RELEASES[-1]}"-centos-7-"${HOST_ARCH}" ceph/daemon:latest-bis
+  make RELEASE="$CONTAINER_BRANCH"-bis FLAVORS="${CEPH_RELEASES[-1]}",centos,7 build
+  docker tag ceph/daemon:"$CONTAINER_BRANCH"-bis-"${CEPH_RELEASES[-1]}"-centos-7-"${HOST_ARCH}" ceph/daemon:latest-bis
   docker push ceph/daemon:latest-bis
 }
 
@@ -182,7 +182,7 @@ function push_ceph_imgs_latests {
       latest_name="${latest_name}-devel"
     fi
     for i in daemon-base daemon; do
-      tag=ceph/$i:${BRANCH}-${LATEST_COMMIT_SHA}-$release-centos-7-${HOST_ARCH}
+      tag=ceph/$i:${CONTAINER_BRANCH}-${CONTAINER_SHA}-$release-centos-7-${HOST_ARCH}
       # tag image
       docker tag "$tag" ceph/$i:"$latest_name"
 

commit 88f361ffed7741ee2f4df5bf2927a4ba794668ea

contrib: fix BRANCH variable name
d4eedeb introduced a regression by omitting a variable renaming.
This breaks the current stable (tagged) container image build.

Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>

contrib/build-push-ceph-container-imgs.sh
@@ -153,7 +153,7 @@ function create_head_or_point_release {
     git checkout refs/tags/"${tag_to_build[*]}"
 
     # find branch associated to that tag
-    BRANCH=$(git branch -r --contains tags/"${tag_to_build[*]}" | grep -Eo 'stable-[0-9].[0-9]')
+    CONTAINER_BRANCH=$(git branch -r --contains tags/"${tag_to_build[*]}" | grep -Eo 'stable-[0-9].[0-9]')
     echo "Building a release Ceph container image based on branch $CONTAINER_BRANCH and tag ${tag_to_build[*]}"
     RELEASE="${tag_to_build[*]}-$CONTAINER_BRANCH"
     # (todo): remove this when we have a better solution like running