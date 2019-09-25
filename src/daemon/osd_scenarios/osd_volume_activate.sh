ceph/ceph-container

commit 90511a4e0231d42d98171ac456e714406605de15

osd_volume_activate: cleanup dmcrypt when osd is stopped
This commit adds dmcrypt cleanup support when osd is stopped.
Otherwise, once a dmcrypt osd is stopped, the resource is still
accessible.

Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

src/daemon/osd_scenarios/osd_volume_activate.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+function get_osd_volume_type {
+  CEPH_VOLUME_LIST_JSON="$(ceph-volume lvm list --format json)"
+  #shellcheck disable=SC2153
+  if echo "$CEPH_VOLUME_LIST_JSON" | $PYTHON -c "import sys, json; print(json.load(sys.stdin)['$OSD_ID'])" &> /dev/null; then
+    echo "lvm"
+  else
+    echo "simple"
+  fi
+}
+
 function osd_volume_simple {
   # Find the devices used by ceph-disk
   DEVICES=$(ceph-volume inventory --format json | $PYTHON -c 'import sys, json; print(" ".join([d.get("path") for d in json.load(sys.stdin) if "Used by ceph-disk" in d.get("rejected_reasons")]))')
@@ -24,10 +34,14 @@ function osd_volume_simple {
 
   # Activate the OSD
   # The command can fail so if it does, let's output the ceph-volume logs
-  if ! ceph-volume simple activate --file ${OSD_JSON} --no-systemd; then
+  if ! ceph-volume simple activate --file "${OSD_JSON}" --no-systemd; then
     cat /var/log/ceph
     exit 1
   fi
+}
+
+function get_dmcrypt_uuids {
+  dmsetup ls --target=crypt | cut -d$'\t' -f 1
 }
 
 function osd_volume_lvm {
@@ -58,9 +72,9 @@ function osd_volume_lvm {
 function osd_volume_activate {
   : "${OSD_ID:?Give me an OSD ID to activate, eg: -e OSD_ID=0}"
 
-  CEPH_VOLUME_LIST_JSON="$(ceph-volume lvm list --format json)"
+  OSD_VOLUME_TYPE=$(get_osd_volume_type)
 
-  if echo "$CEPH_VOLUME_LIST_JSON" | $PYTHON -c "import sys, json; print(json.load(sys.stdin)[\"$OSD_ID\"])" &> /dev/null; then
+  if [[ "$OSD_VOLUME_TYPE" == "lvm" ]]; then
     osd_volume_lvm
   else
     osd_volume_simple
@@ -77,6 +91,20 @@ function osd_volume_activate {
     for mnt in $ceph_mnt; do
       log "osd_volume_activate: Unmounting $mnt"
       umount "$mnt" || (log "osd_volume_activate: Failed to umount $mnt"; lsof "$mnt")
+    done
+
+    UUIDS=$(get_dmcrypt_uuids)
+
+    for uuid in ${UUIDS}; do
+      if [[ "$OSD_VOLUME_TYPE" == "simple" ]]; then
+        DATA="${OSD_JSON}"
+      else
+        DATA=$(echo "$CEPH_VOLUME_LIST_JSON" | $PYTHON -c "import sys, json; print(json.load(sys.stdin)['$OSD_ID'])")
+      fi
+      if grep -qo "${uuid}" "${DATA}"; then
+        log "osd_volume_activate: Closing dmcrypt $uuid"
+        cryptsetup close "${uuid}" || log "osd_volume_activate: Failed to close dmcrypt ${uuid}"
+      fi
     done
   }
   exec /usr/bin/ceph-osd "${DAEMON_OPTS[@]}" -i "${OSD_ID}"

commit d86335fd8f31891ded3aebe502a62b963aa7e073

osd_volume_activate: fix osd_volume_lvm function
90511a4e0231d42d98171ac456e714406605de15 introduced a bug in
osd_volume_lvm function.
OSDs couldn't start because `$CEPH_VOLUME_LIST_JSON` is seen as an empty
variable when consumed in this function.
Since there's no need to have a separate function for detecting the
volume type, let's refact this part of the code so we can fix this
issue.

Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

src/daemon/osd_scenarios/osd_volume_activate.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -e
 
-function get_osd_volume_type {
-  CEPH_VOLUME_LIST_JSON="$(ceph-volume lvm list --format json)"
-  #shellcheck disable=SC2153
-  if echo "$CEPH_VOLUME_LIST_JSON" | $PYTHON -c "import sys, json; print(json.load(sys.stdin)['$OSD_ID'])" &> /dev/null; then
-    echo "lvm"
-  else
-    echo "simple"
-  fi
-}
-
 function osd_volume_simple {
   # Find the devices used by ceph-disk
   DEVICES=$(ceph-volume inventory --format json | $PYTHON -c 'import sys, json; print(" ".join([d.get("path") for d in json.load(sys.stdin) if "Used by ceph-disk" in d.get("rejected_reasons")]))')
@@ -26,6 +16,7 @@ function osd_volume_simple {
   done
 
   # Find the OSD json file associated to the ID
+  #shellcheck disable=SC2153
   OSD_JSON=$(grep -l "whoami\": ${OSD_ID}$" /etc/ceph/osd/*.json)
   if [ -z "${OSD_JSON}" ]; then
     log "OSD id ${OSD_ID} does not exist"
@@ -46,10 +37,10 @@ function get_dmcrypt_uuids {
 
 function osd_volume_lvm {
   # Find the OSD FSID from the OSD ID
-  OSD_FSID="$(echo "$CEPH_VOLUME_LIST_JSON" | $PYTHON -c "import sys, json; print(json.load(sys.stdin)[\"$OSD_ID\"][0][\"tags\"][\"ceph.osd_fsid\"])")"
+  OSD_FSID="$(echo "$CEPH_VOLUME_LIST_JSON" | $PYTHON -c "import sys, json; print(json.load(sys.stdin)['$OSD_ID'][0]['tags']['ceph.osd_fsid'])")"
 
   # Find the OSD type
-  OSD_TYPE="$(echo "$CEPH_VOLUME_LIST_JSON" | $PYTHON -c "import sys, json; print(json.load(sys.stdin)[\"$OSD_ID\"][0][\"type\"])")"
+  OSD_TYPE="$(echo "$CEPH_VOLUME_LIST_JSON" | $PYTHON -c "import sys, json; print(json.load(sys.stdin)['$OSD_ID'][0]['type'])")"
 
   # Discover the objectstore
   if [[ "data journal" =~ $OSD_TYPE ]]; then
@@ -72,7 +63,14 @@ function osd_volume_lvm {
 function osd_volume_activate {
   : "${OSD_ID:?Give me an OSD ID to activate, eg: -e OSD_ID=0}"
 
-  OSD_VOLUME_TYPE=$(get_osd_volume_type)
+  CEPH_VOLUME_LIST_JSON="$(ceph-volume lvm list --format json)"
+
+  #shellcheck disable=SC2153
+  if echo "$CEPH_VOLUME_LIST_JSON" | $PYTHON -c "import sys, json; print(json.load(sys.stdin)['$OSD_ID'])" &> /dev/null; then
+    OSD_VOLUME_TYPE=lvm
+  else
+    OSD_VOLUME_TYPE=simple
+  fi
 
   if [[ "$OSD_VOLUME_TYPE" == "lvm" ]]; then
     osd_volume_lvm