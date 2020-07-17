ceph/ceph-container

commit 3b59e1044862ca438ac50b46922de0150e0144bb

build-push-ceph-container-imgs.sh: change container tag
with teuthology supporting crimson flavor build differentiating
container on basis of only sha1 is not unique anymore.
This change will push container with tag format:

<>:sha1-shaman_osd_flavor

Signed-off-by: Deepika Upadhyay <dupadhya@redhat.com>

contrib/build-push-ceph-container-imgs.sh
@@ -20,6 +20,7 @@ if ${CI_CONTAINER} ; then
   # must be set by caller (perhaps another Jenkins build job)
   # BRANCH (as above, the Ceph branch)
   # SHA1 (sha1 corresponding to the Ceph branch)
+  # OSD_FLAVOR (choose between default and crimson flavor)
   # CONTAINER_REPO_HOSTNAME="quay.io"
   # CONTAINER_REPO_ORGANIZATION="ceph-ci"
   # CONTAINER_REPO_USERNAME=user
@@ -39,7 +40,8 @@ CONTAINER_BRANCH="${GIT_BRANCH#*/}"
 CONTAINER_SHA=$(git rev-parse --short HEAD)
 TAGGED_HEAD=false # does HEAD is on a tag ?
 DEVEL=${DEVEL:=false}
-OSD_FLAVOR=${FLAVOR:="default"}
+# flavor based on OSD type proporgated by ceph-build
+OSD_FLAVOR=${OSD_FLAVOR:=default}
 
 if [ -z "$CEPH_RELEASES" ]; then
   # NEVER change 'master' position in the array, this will break the 'latest' tag
@@ -255,6 +257,11 @@ function push_ceph_imgs_latest {
     docker push $full_repo_tag
     docker push $branch_repo_tag
     docker push $sha1_repo_tag
+    if [[ "${OSD_FLAVOR}" == "crimson" ]]; then
+      sha1_flavor_repo_tag=${CONTAINER_REPO_HOSTNAME}/${CONTAINER_REPO_ORGANIZATION}/ceph:${SHA1}-${OSD_FLAVOR}
+      docker tag $local_tag $sha1_flavor_repo_tag
+      docker push $sha1_flavor_repo_tag
+    fi
     return
   fi
 

commit 8209440828a105d155f6c70123a00460bf935e00

build-push-ceph-container-imgs.sh: do not overwrite images created for default flavor
otherwise cephadm and other consumers pulling using $full_repo_tag,
$branch_repo_tag and $sha1_repo_tag will be testing crimson-osd, if
crimson-osd flavored image is pushed after the default one.

this change addresses a regression introduced by 3b59e10448

Fixes: https://github.com/ceph/ceph-container/pull/1712
Signed-off-by: Kefu Chai <kchai@redhat.com>

contrib/build-push-ceph-container-imgs.sh
@@ -251,16 +251,17 @@ function push_ceph_imgs_latest {
     full_repo_tag=${CONTAINER_REPO_HOSTNAME}/${CONTAINER_REPO_ORGANIZATION}/ceph:${RELEASE}-centos-${CENTOS_RELEASE}-${HOST_ARCH}-devel
     branch_repo_tag=${CONTAINER_REPO_HOSTNAME}/${CONTAINER_REPO_ORGANIZATION}/ceph:${BRANCH}
     sha1_repo_tag=${CONTAINER_REPO_HOSTNAME}/${CONTAINER_REPO_ORGANIZATION}/ceph:${SHA1}
-    docker tag $local_tag $full_repo_tag
-    docker tag $local_tag $branch_repo_tag
-    docker tag $local_tag $sha1_repo_tag
-    docker push $full_repo_tag
-    docker push $branch_repo_tag
-    docker push $sha1_repo_tag
     if [[ "${OSD_FLAVOR}" == "crimson" ]]; then
       sha1_flavor_repo_tag=${CONTAINER_REPO_HOSTNAME}/${CONTAINER_REPO_ORGANIZATION}/ceph:${SHA1}-${OSD_FLAVOR}
       docker tag $local_tag $sha1_flavor_repo_tag
       docker push $sha1_flavor_repo_tag
+    else
+      docker tag $local_tag $full_repo_tag
+      docker tag $local_tag $branch_repo_tag
+      docker tag $local_tag $sha1_repo_tag
+      docker push $full_repo_tag
+      docker push $branch_repo_tag
+      docker push $sha1_repo_tag
     fi
     return
   fi