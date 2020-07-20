5fd161419964ee92ab5e44810ef0d2e7581a5d11
Kefu Chai <kchai@redhat.com>
2020-07-20 14:04:46 +0800

    maint-lib/ceph_version: extract parse_ceph_version_spec into a function
    so we can reuse it in another function in a following change, which
    extracts the ceph_version out of version_spec.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- maint-lib/ceph_version.sh
+++ maint-lib/ceph_version.sh
@@ -9,33 +9,40 @@ WHAT_TO_EXTRACT=$2
 CEPH_POINT_RELEASE=""
 CEPH_VERSION="${CEPH_VERSION_SPEC}"
 
+function parse_ceph_version_spec {
+  local version_spec="${1}"
+  shift
+  local what_to_extract="${1}"
+  shift
+  # Search for the two possible separators between CEPH_VERSION and CEPH_POINT_RELEASE
+  # Let's consider CEPH_VERSION_SPEC=luminous-12.2.0-1
+  for separator in "=" "-"; do
+    # If the line doesn't have a separator, let's try the next one
+    if [[ ! "${version_spec}" =~ ${separator} ]]; then continue; fi
+
+    # If we found it, let's save both parts in the respective variables
+    # shellcheck disable=SC2034
+    CEPH_VERSION="${CEPH_VERSION_SPEC%%${separator}*}"
+    CEPH_POINT_RELEASE="${separator}${CEPH_VERSION_SPEC#*${separator}}"
+    # Don't continue if we found something
+    break
+  done
+  # Let's print the requested variable
+  echo "${!what_to_extract}"
+}
+
 # If we pass a dev branch, we don't know the version so let's use the branch name as the ceph version
 case "$WHAT_TO_EXTRACT" in
   CEPH_POINT_RELEASE)
     if [[ $CEPH_VERSION =~ ^wip* ]]; then
       echo $CEPH_POINT_RELEASE
+    else
+      echo $(parse_ceph_version_spec ${CEPH_VERSION_SPEC} CEPH_POINT_RELEASE)
     fi;;
   CEPH_VERSION)
     if [[ $CEPH_VERSION =~ ^wip* ]]; then
       echo "$CEPH_VERSION"
+    else
+      echo $(parse_ceph_version_spec ${CEPH_VERSION_SPEC} CEPH_VERSION)
     fi;;
 esac
-
-# Search for the two possible separators between CEPH_VERSION and CEPH_POINT_RELEASE
-# Let's consider CEPH_VERSION_SPEC=luminous-12.2.0-1
-for separator in "=" "-"; do
-  # If the line doesn't have a separator, let's try the next one
-  if [[ ! "${CEPH_VERSION_SPEC}" =~ ${separator} ]]; then continue; fi
-
-  # If we found it, let's save both parts in the respective variables
-  # shellcheck disable=SC2034
-  CEPH_VERSION="${CEPH_VERSION_SPEC%%${separator}*}"
-  # shellcheck disable=SC2034
-  CEPH_POINT_RELEASE="${separator}${CEPH_VERSION_SPEC#*${separator}}"
-
-  # Don't continue if we found something, it's time to print it
-  break
-done
-
-# Let's print the requested variable
-echo "${!WHAT_TO_EXTRACT}"
