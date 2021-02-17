66350e1b7a1d12c832cbe5d04c4245d094b84c3a
Dan Mick <dmick@redhat.com>
2021-02-16 16:42:28 -0800

    contrib/build-push-ceph-container-images.sh: don't push short tags for arm
    Currently, using normal manifests/tags as we are, the 'branch' and
    'sha1' tags collide for x86_64 and arm64.  While the solution for this
    is probably to create 'manifest lists' or 'fat manifests' for those
    tags, and point to both archs, the way release containers do,
    until we can get there (and modify prune-quay.py to handle the new
    form of tags as well), this will allow us to build containers
    with the full tag at least, and not tie the build speed of both
    arch builders together.
    
    Signed-off-by: Dan Mick <dmick@redhat.com>

--- contrib/build-push-ceph-container-imgs.sh
+++ contrib/build-push-ceph-container-imgs.sh
@@ -292,19 +292,23 @@ function push_ceph_imgs_latest {
     branch_repo_tag=${CONTAINER_REPO_HOSTNAME}/${CONTAINER_REPO_ORGANIZATION}/ceph:${BRANCH}
     sha1_repo_tag=${CONTAINER_REPO_HOSTNAME}/${CONTAINER_REPO_ORGANIZATION}/ceph:${SHA1}
     if [[ "${OSD_FLAVOR}" == "crimson" ]]; then
-      sha1_flavor_repo_tag=${CONTAINER_REPO_HOSTNAME}/${CONTAINER_REPO_ORGANIZATION}/ceph:${SHA1}-${OSD_FLAVOR}
-      docker tag "$local_tag" "$sha1_flavor_repo_tag"
-      docker push "$sha1_flavor_repo_tag"
+      if [[ "${HOST_ARCH}" == "x86_64" ]]; then
+        sha1_flavor_repo_tag=${CONTAINER_REPO_HOSTNAME}/${CONTAINER_REPO_ORGANIZATION}/ceph:${SHA1}-${OSD_FLAVOR}
+        docker tag "$local_tag" "$sha1_flavor_repo_tag"
+        docker push "$sha1_flavor_repo_tag"
+      fi
     elif [[ "${distro_release}" == "7" ]]; then
       docker tag "$local_tag" "$full_repo_tag"
       docker push "$full_repo_tag"
     else
       docker tag "$local_tag" "$full_repo_tag"
-      docker tag "$local_tag" "$branch_repo_tag"
-      docker tag "$local_tag" "$sha1_repo_tag"
       docker push "$full_repo_tag"
-      docker push "$branch_repo_tag"
-      docker push "$sha1_repo_tag"
+      if [[ "${HOST_ARCH}" == "x86_64" ]] ; then
+        docker tag "$local_tag" "$branch_repo_tag"
+        docker tag "$local_tag" "$sha1_repo_tag"
+        docker push "$branch_repo_tag"
+        docker push "$sha1_repo_tag"
+      fi
     fi
     return
   fi
