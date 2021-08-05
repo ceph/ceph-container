80a5928ce73854d1b3b7126a7f6b15549b3d84b3
Dimitri Savineau <dsavinea@redhat.com>
2021-08-05 14:10:54 -0400

    Drop old ceph releases
    We don't build those old ceph releases so we can do some cleanup.
    
    Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>

--- src/daemon/start_mon.sh
+++ src/daemon/start_mon.sh
@@ -183,13 +183,11 @@ function start_mon {
 
   # start MON
   if [[ "$CEPH_DAEMON" == demo ]]; then
-    if [[ ! "${CEPH_VERSION}" =~ ^(luminous|mimic)$ ]]; then
-      if ! grep -qE "mon warn on pool no redundancy = false" /etc/ceph/"${CLUSTER}".conf; then
-          echo "mon warn on pool no redundancy = false" >> /etc/ceph/"${CLUSTER}".conf
-      fi
-      if ! grep -qE "auth allow insecure global id reclaim = false" /etc/ceph/"${CLUSTER}".conf; then
-          echo "auth allow insecure global id reclaim = false" >> /etc/ceph/"${CLUSTER}".conf
-      fi
+    if ! grep -qE "mon warn on pool no redundancy = false" /etc/ceph/"${CLUSTER}".conf; then
+        echo "mon warn on pool no redundancy = false" >> /etc/ceph/"${CLUSTER}".conf
+    fi
+    if ! grep -qE "auth allow insecure global id reclaim = false" /etc/ceph/"${CLUSTER}".conf; then
+        echo "auth allow insecure global id reclaim = false" >> /etc/ceph/"${CLUSTER}".conf
     fi
     /usr/bin/ceph-mon "${DAEMON_OPTS[@]}" -i "${MON_NAME}" --mon-data "$MON_DATA_DIR" --public-addr "${MON_IP}"
 
