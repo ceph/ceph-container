7c5f7e3eea521b1488b0bf1bf2314881a1aba886
Dimitri Savineau <dsavinea@redhat.com>
2021-11-04 13:27:15 -0400

    daemon: remove forego usage
    This removes the forego installation and osd scenarios using it since
    it isn't maintained and used anymore.
    
    Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>

--- src/daemon/osd_scenarios/osd_common.sh
+++ src/daemon/osd_scenarios/osd_common.sh
@@ -2,10 +2,7 @@
 set -e
 
 # Start the latest OSD
-# In case of forego, we don't run ceph-osd, start_forego will do it later
 start_osd() {
-  mode=$1 #forego or empty
-
   OSD_ID=$(find /var/lib/ceph/osd/ -maxdepth 1 -mindepth 1 -printf '%TY-%Tm-%Td %TT %p\n' | sort -n | tail -n1 | awk -F '-' -v pattern="$CLUSTER" '$0 ~ pattern {print $4}')
 
   # ceph-disk activiate has exec'ed /usr/bin/ceph-osd ${CLI_OPTS[@]} -f -i ${OSD_ID}
@@ -16,15 +13,6 @@ start_osd() {
       while [ -e /proc/"${OSD_PID}" ]; do sleep 1;done
   fi
 
-  if [ "$mode" = "forego" ]; then
-   echo "${CLUSTER}-${OSD_ID}: /usr/bin/ceph-osd ${CLI_OPTS[*]} -f -i ${OSD_ID} --setuser ceph --setgroup disk" | tee -a /etc/forego/"${CLUSTER}"/Procfile
-  else
-   log "SUCCESS"
-   exec /usr/bin/ceph-osd "${CLI_OPTS[@]}" -f -i "${OSD_ID}" --setuser ceph --setgroup disk
-  fi
-}
-
-# Starting forego
-start_forego() {
-   exec /usr/local/bin/forego start -f /etc/forego/"${CLUSTER}"/Procfile
+  log "SUCCESS"
+  exec /usr/bin/ceph-osd "${CLI_OPTS[@]}" -f -i "${OSD_ID}" --setuser ceph --setgroup disk
 }
