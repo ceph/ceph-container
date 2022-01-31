167eb6383b9ef11ec42f5320fd1176d905d13396
Guillaume Abrioux <gabrioux@redhat.com>
2022-01-31 21:32:14 +0100

    build: update contrib scripts
    This is needed so the CI can build&push the images.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>
    Co-authored-by: Dimitri Savineau <dsavinea@redhat.com>
    (cherry picked from commit a1b87761403fce59838310ee5754d1dc8352cd45)

--- contrib/build-push-ceph-container-imgs-arm64.sh
+++ contrib/build-push-ceph-container-imgs-arm64.sh
@@ -22,8 +22,8 @@ function install_docker {
 function build_ceph_imgs {
   echo "Build Ceph container image(s)"
   for ceph_release in "${CEPH_RELEASES[@]}"; do
-    CENTOS_RELEASE=$(_centos_release ${ceph_release})
-    make DAEMON_BASE_TAG=daemon-base:"$RELEASE"-"${ceph_release}"-centos-${CENTOS_RELEASE}-aarch64 DAEMON_TAG=daemon:"$RELEASE"-"${ceph_release}"-centos-${CENTOS_RELEASE}-aarch64 RELEASE="$RELEASE" FLAVORS="${ceph_release},centos-arm64,${CENTOS_RELEASE}" BASEOS_REPO=centos build
+    CENTOS_RELEASE=$(_centos_release "${ceph_release}")
+    make BASEOS_TAG=stream"${CENTOS_RELEASE}" DAEMON_BASE_TAG=daemon-base:"$RELEASE"-"${ceph_release}"-centos-"${CENTOS_RELEASE}"-aarch64 DAEMON_TAG=daemon:"$RELEASE"-"${ceph_release}"-centos-"${CENTOS_RELEASE}"-aarch64 RELEASE="${RELEASE}" FLAVORS="${ceph_release},centos-arm64,${CENTOS_RELEASE}" BASEOS_REGISTRY="${CONTAINER_REPO_HOSTNAME}/centos" BASEOS_REPO=centos TAG_REGISTRY="${CONTAINER_REPO_ORGANIZATION}" build
   done
   docker images
 }
@@ -31,8 +31,8 @@ function build_ceph_imgs {
 function push_ceph_imgs {
   echo "Push Ceph container image(s) to the Docker Hub registry"
   for ceph_release in "${CEPH_RELEASES[@]}"; do
-    CENTOS_RELEASE=$(_centos_release ${ceph_release})
-    make DAEMON_BASE_TAG=daemon-base:"$RELEASE"-"${ceph_release}"-centos-${CENTOS_RELEASE}-aarch64 DAEMON_TAG=daemon:"$RELEASE"-"${ceph_release}"-centos-${CENTOS_RELEASE}-aarch64 RELEASE="$RELEASE" FLAVORS="${ceph_release},centos-arm64,${CENTOS_RELEASE}" BASEOS_REPO=centos push
+    CENTOS_RELEASE=$(_centos_release "${ceph_release}")
+    make BASEOS_TAG=stream"${CENTOS_RELEASE}" DAEMON_BASE_TAG=daemon-base:"$RELEASE"-"${ceph_release}"-centos-"${CENTOS_RELEASE}"-aarch64 DAEMON_TAG=daemon:"$RELEASE"-"${ceph_release}"-centos-"${CENTOS_RELEASE}"-aarch64 RELEASE="${RELEASE}" FLAVORS="${ceph_release},centos-arm64,${CENTOS_RELEASE}" BASEOS_REGISTRY="${CONTAINER_REPO_HOSTNAME}/centos" BASEOS_REPO=centos TAG_REGISTRY="${CONTAINER_REPO_ORGANIZATION}" push
   done
 }
 
