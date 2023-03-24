ec04edfc3e41ea2cf2b756f8c7915a012937506e
Guillaume Abrioux <gabrioux@redhat.com>
2023-03-24 12:49:46 +0100

    contrib: various fixes
    This modifies commit-rhcs.sh and compose-rhcs.sh.
    The composed dir path was hardcoded, it is now generated automatically
    depending on the value of the env var `VERSION`.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

---
+++ contrib/common.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# shellcheck disable=SC2034
+UBI_BRANDING=${BRANDING:-ibm}
+
+case "${VERSION}" in
+  *4*)
+    UBI_VERSION=8
+    CEPH_RELEASE=nautilus
+    ;;
+  *5*)
+    UBI_VERSION=8
+    CEPH_RELEASE=pacific
+    ;;
+  *6*)
+    UBI_VERSION=9
+    CEPH_RELEASE=quincy
+    ;;
+    *)
+    echo "ERROR: VERSION must be set to a valid version."
+    exit 1
+esac
+
