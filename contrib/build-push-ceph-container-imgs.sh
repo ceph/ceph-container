30cbd16135e90fa5331c5a38ccd3c510f54c41ae
Guillaume Abrioux <gabrioux@ibm.com>
2024-02-14 01:11:52 +0100

    Revert "squid: Update contrib/build-push-ceph-container-imgs.sh with the new release"
    This reverts commit e51f1c7ca6fd4e2fa3eea407716e8ad3fe7e7fff.
    
    This change doesn't make sense as no squid RPMs are available at the
    moment. Given that it makes the jenkins job fail, let's revert it.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

--- contrib/build-push-ceph-container-imgs.sh
+++ contrib/build-push-ceph-container-imgs.sh
@@ -51,7 +51,7 @@ OSD_FLAVOR=${OSD_FLAVOR:=default}
 
 if [ -z "$CEPH_RELEASES" ]; then
   # NEVER change 'main' position in the array, this will break the 'latest' tag
-  CEPH_RELEASES=(main pacific quincy reef squid)
+  CEPH_RELEASES=(main pacific quincy reef)
 fi
 
 HOST_ARCH=$(uname -m)
