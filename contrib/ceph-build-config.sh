3433ed143c315c9e091fecc8a1f769e9dca1ce8b
Dan Mick <dmick@redhat.com>
2024-05-31 13:36:40 -0700

    Remove custom c9 __DOCKERFILE_INSTALL__; use generic version
    The c9 version didn't get the prerelease changes for ceph-release,
    so something was necessary to add them; besides that, the only
    differences were the old method of selecting ganesha for later
    releases and the enablement of crb instead of powertools in the
    last line. Since we already had code to test centos8 in
    daemon-base/__DOCKERFILE_INSTALL__, one more small conditional
    won't hurt.
    
    Also add the projectname (ceph) to the prerelease path (since
    ceph-build's sync-push tool now adds it).  This affects
    the __DOCKERFILE_INSTALL_ as well as contrib/ceph-build-config.sh
    
    Signed-off-by: Dan Mick <dmick@redhat.com>

--- contrib/ceph-build-config.sh
+++ contrib/ceph-build-config.sh
@@ -227,7 +227,7 @@ function get_ceph_download_url () {
       error "get_ceph_download_url - unknown distro '${distro}''"
   esac
   if [ "$PRERELEASE" = true ] ; then
-    echo "https://$PRERELEASE_USERNAME:$PRERELEASE_PASSWORD@download.ceph.com/prerelease/${flavor_path}/${arch}/"
+    echo "https://$PRERELEASE_USERNAME:$PRERELEASE_PASSWORD@download.ceph.com/prerelease/ceph/${flavor_path}/${arch}/"
   else
     echo "https://download.ceph.com/${flavor_path}/${arch}/"
   fi
@@ -376,6 +376,7 @@ function get_latest_full_semver_tag () {
   # For searching full versions, always use 'v<major>.<minor>.<bug>-' including the dash so that a
   # search for 'v1.1.1' doesn't return 'v1.1.11' for example
   if [ -n "${TEST_RUN:-}" ]; then
+    # date %Y%m%d
     local build_num ; build_num="$(generate_new_build_number)"
     build_num=$((build_num - 1))
     if [[ $repository =~ .*amd64.* ]]; then
