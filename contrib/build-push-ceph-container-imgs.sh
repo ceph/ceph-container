34dd446b83a51282d3decc2f0b4786b39b303d2d
Guillaume Abrioux <gabrioux@ibm.com>
2024-06-06 09:05:57 +0200

    do not push daemon image
    This image is no longer built, any try to push it will fail.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

--- contrib/build-push-ceph-container-imgs.sh
+++ contrib/build-push-ceph-container-imgs.sh
@@ -331,7 +331,7 @@ function push_ceph_imgs_latest {
     if ${DEVEL}; then
       latest_name="${latest_name}-devel"
     fi
-    for i in daemon-base daemon demo; do
+    for i in daemon-base demo; do
       tag=${CONTAINER_REPO_ORGANIZATION}/$i:${CONTAINER_BRANCH}-${CONTAINER_SHA}-$release-centos-stream$(_centos_release "${release}")-${HOST_ARCH}
       # tag image
       docker tag "$tag" "${CONTAINER_REPO_ORGANIZATION}"/$i:"$latest_name"
